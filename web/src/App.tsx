8f2abf8eb18c383e271895c6ede2d1a138bdf55f
Evan Gray <battledingo@gmail.com>
2022-09-18 17:55:29 -0400

    web: add to tilt

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,7 +7,9 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
+import { ErrorBoundary } from "react-error-boundary";
 import CustomThemeProvider from "./components/CustomThemeProvider";
+import ErrorFallback from "./components/ErrorFallback";
 import Main from "./components/Main";
 import NetworkSelector from "./components/NetworkSelector";
 import Settings from "./components/Settings";
@@ -17,23 +19,25 @@ import WormholeStatsIcon from "./icons/WormholeStatsIcon";
 
 function App() {
   return (
-    <SettingsContextProvider>
-      <CustomThemeProvider>
-        <CssBaseline />
-        <NetworkContextProvider>
-          <AppBar position="static">
-            <Toolbar variant="dense">
-              <Box pr={1} display="flex" alignItems="center">
-                <WormholeStatsIcon />
-              </Box>
-              <Typography variant="h6">Explorer</Typography>
-              <Box flexGrow={1} />
-            </Toolbar>
-          </AppBar>
-          <Main />
-        </NetworkContextProvider>
-      </CustomThemeProvider>
-    </SettingsContextProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <SettingsContextProvider>
+        <CustomThemeProvider>
+          <CssBaseline />
+          <NetworkContextProvider>
+            <AppBar position="static">
+              <Toolbar variant="dense">
+                <Box pr={1} display="flex" alignItems="center">
+                  <WormholeStatsIcon />
+                </Box>
+                <Typography variant="h6">Explorer</Typography>
+                <Box flexGrow={1} />
+              </Toolbar>
+            </AppBar>
+            <Main />
+          </NetworkContextProvider>
+        </CustomThemeProvider>
+      </SettingsContextProvider>
+    </ErrorBoundary>
   );
 }
 
---
+++ web/src/components/ErrorFallback.tsx
@@ -0,0 +1,12 @@
+import { Button, Typography } from "@mui/material";
+
+function ErrorFallback({ error }: { error: { message: string } }) {
+  return (
+    <div role="alert">
+      <Typography>Something went wrong:</Typography>
+      <pre>{error.message}</pre>
+    </div>
+  );
+}
+
+export default ErrorFallback;
