e5df34b702f1338db43d452b28fd57f8ca6558ad
Gabriel Zimmermann <gabrielemanuel@gmail.com>
2022-10-22 21:58:38 -0300

    refactors storage into separate layer

---
+++ fly/storage/db.go
@@ -0,0 +1,24 @@
+package storage
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.uber.org/zap"
+	"time"
+)
+
+// GetDB connects to DB and returns a client that will disconnect when the passed in context is cancelled
+func GetDB(appCtx context.Context, log *zap.Logger, uri, databaseName string) (*mongo.Database, error) {
+	cli, err := mongo.Connect(appCtx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		<-appCtx.Done()
+		ctx, _ := context.WithDeadline(context.Background(), time.Now().Add(30*time.Second))
+		err := cli.Disconnect(ctx)
+		log.Error("error disconnecting from db", zap.Error(err))
+	}()
+	return cli.Database(databaseName), err
+}
