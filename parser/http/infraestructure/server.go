2283e84ed63c2e7e1e38634dd87c726765810c29
walker-16 <agpazos85@gmail.com>
2023-01-23 16:40:39 -0300

    Enabled pprof parser component (#83)

--- parser/http/infraestructure/server.go
+++ parser/http/infraestructure/server.go
@@ -3,6 +3,7 @@ package infraestructure
 import (
 	"github.com/gofiber/fiber/v2"
 	fiberLog "github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/pprof"
 	"github.com/wormhole-foundation/wormhole-explorer/parser/internal/sqs"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
@@ -14,17 +15,24 @@ type Server struct {
 	logger *zap.Logger
 }
 
-func NewServer(logger *zap.Logger, port string, isQueueConsumer bool, consumer *sqs.Consumer, db *mongo.Database) *Server {
+func NewServer(logger *zap.Logger, port string, pprofEnabled bool, isQueueConsumer bool, consumer *sqs.Consumer, db *mongo.Database) *Server {
 	repository := NewRepository(db, logger)
 	service := NewService(repository, consumer, isQueueConsumer, logger)
 	ctrl := NewController(service)
 	app := fiber.New()
+
+	// config use of middlware.
+	if pprofEnabled {
+		app.Use(pprof.New())
+	}
 	app.Use(fiberLog.New(fiberLog.Config{
 		Format: "level=info timestamp=${time} method=${method} path=${path} status${status} request_id=${locals:requestid}\n",
 	}))
+
 	api := app.Group("/api")
 	api.Get("/health", ctrl.HealthCheck)
 	api.Get("/ready", ctrl.ReadyCheck)
+
 	return &Server{
 		app:    app,
 		port:   port,
