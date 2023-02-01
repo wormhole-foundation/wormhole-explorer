d32405f7379f4d7f264daeb5091698ffb7e4e9d5
agodnic <agustingodnic@gmail.com>
2023-02-01 14:11:40 -0300

    [API] Update swagger docs (#120)
    Co-authored-by: Agustin Godnic <agustin@xlabs.xyz>

--- api/routes/wormscan/infrastructure/controller.go
+++ api/routes/wormscan/infrastructure/controller.go
@@ -16,6 +16,7 @@ func NewController(serv *infrastructure.Service) *Controller {
 	return &Controller{srv: serv}
 }
 
+// HealthCheck is the HTTP route handler for the endpoint `GET /api/v1/health`.
 // HealthCheck godoc
 // @Description Health check
 // @Tags Wormscan
@@ -30,12 +31,12 @@ func (c *Controller) HealthCheck(ctx *fiber.Ctx) error {
 	}{Status: "OK"})
 }
 
-// ReadyCheck handler for the endpoint /ready
+// ReadyCheck is the HTTP handler for the endpoint `GET /api/v1/ready`.
 // ReadyCheck godoc
 // @Description Ready check
 // @Tags Wormscan
 // @ID ready-check
-// @Success 200 {object} object{status=string}
+// @Success 200 {object} object{ready=string}
 // @Failure 400
 // @Failure 500
 // @Router /api/v1/ready [get]
@@ -51,26 +52,30 @@ func (c *Controller) ReadyCheck(ctx *fiber.Ctx) error {
 	}{Ready: "NO"})
 }
 
-// ReadyCheck handler for the endpoint /ready
-// ReadyCheck godoc
-// @Description Ready check
+// VersionResponse is the JSON model for the 200 OK response in `GET /api/v1/version`.
+type VersionResponse struct {
+	BuildDate string `json:"buildDate"`
+	Build     string `json:"build"`
+	Branch    string `json:"branch"`
+	Machine   string `json:"machine"`
+	User      string `json:"user"`
+}
+
+// Version is the HTTP route handler for the endpoint `GET /api/v1/version`.
+// Version godoc
+// @Description Get version/release information.
 // @Tags Wormscan
-// @ID ready-check
-// @Success 200 {object} object{status=string}
+// @ID get-version
+// @Success 200 {object} VersionResponse
 // @Failure 400
 // @Failure 500
 // @Router /api/v1/version [get]
 func (c *Controller) Version(ctx *fiber.Ctx) error {
-	return ctx.JSON(struct {
-		BuildDate string `json:"build_date"`
-		Build     string `json:"build"`
-		Branch    string `json:"branch"`
-		Machine   string `json:"machine"`
-		User      string `json:"user"`
-	}{BuildDate: build.Time,
-		Branch:  build.Branch,
-		Build:   build.Build,
-		Machine: build.Machine,
-		User:    build.User},
-	)
+	return ctx.JSON(VersionResponse{
+		BuildDate: build.Time,
+		Branch:    build.Branch,
+		Build:     build.Build,
+		Machine:   build.Machine,
+		User:      build.User,
+	})
 }
