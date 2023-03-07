1f38f00de91086e2a7f789fde676a47c16a14268
Agustin Godnic <agustin@xlabs.xyz>
2023-03-07 15:10:15 -0300

    [TX TRACKER] Enable support for Solana

--- txtracker/chains/solanaRpc.go
+++ txtracker/chains/solanaRpc.go
@@ -4,14 +4,47 @@ import (
 	"context"
 	"encoding/hex"
 	"fmt"
-	"math/big"
 	"time"
 
+	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/mr-tron/base58"
-	"github.com/portto/solana-go-sdk/client"
 
 	"github.com/wormhole-foundation/wormhole-explorer/txtracker/config"
 )
+
+type solanaTransactionSignature struct {
+	Signature string `json:"signature"`
+}
+
+type solanaGetTransactionResponse struct {
+	BlockTime int64                 `json:"blockTime"`
+	Meta      solanaTransactionMeta `json:"meta"`
+}
+
+type solanaTransactionMeta struct {
+	InnerInstructions []solanaInnerInstruction `json:"innerInstructions"`
+}
+
+type solanaInnerInstruction struct {
+	Instructions []solanaInstruction `json:"instructions"`
+}
+
+type solanaInstruction struct {
+	ParsedInstruction solanaParsedInstruction `json:"parsed"`
+}
+
+type solanaParsedInstruction struct {
+	Type_ string                      `json:"type"`
+	Info  solanaParsedInstructionInfo `json:"info"`
+}
+
+type solanaParsedInstructionInfo struct {
+	Account     string `json:"account"`
+	Amount      string `json:"amount"`
+	Authority   string `json:"authority"`
+	Destination string `json:"destination"`
+	Source      string `json:"source"`
+}
 
 func fetchSolanaTx(
 	ctx context.Context,
@@ -19,7 +52,12 @@ func fetchSolanaTx(
 	txHash string,
 ) (*TxDetail, error) {
 
-	c := client.NewClient(cfg.SolanaBaseUrl)
+	// Initialize RPC client
+	client, err := rpc.DialContext(ctx, cfg.SolanaBaseUrl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize RPC client: %w", err)
+	}
+	defer client.Close()
 
 	// Decode txHash bytes
 	h, err := hex.DecodeString(txHash)
@@ -28,81 +66,43 @@ func fetchSolanaTx(
 	}
 
 	// Get transaction signatures for the given account
-	sigs, err := c.GetSignaturesForAddress(ctx, base58.Encode(h))
+	var sigs []solanaTransactionSignature
+	err = client.CallContext(ctx, &sigs, "getSignaturesForAddress", base58.Encode(h))
 	if err != nil {
-		return nil, fmt.Errorf("failed to get signatures for txHash=%s: %w", txHash, err)
+		return nil, fmt.Errorf("failed to get signatures for account: %w", err)
 	}
 	if len(sigs) != 1 {
-		return nil, fmt.Errorf("expected account to have exactly one signature, but found %d", len(sigs))
+		return nil, fmt.Errorf("expected exactly one signature, but found %d", len(sigs))
 	}
 
 	// Fetch the portal token bridge transaction
-	tx, err := c.GetTransaction(ctx, sigs[0].Signature)
+	var response solanaGetTransactionResponse
+	err = client.CallContext(ctx, &response, "getTransaction", sigs[0].Signature, "jsonParsed")
 	if err != nil {
-		return nil, fmt.Errorf("failed to get transaction sig=%s, err: %v", sigs[0].Signature, err)
+		return nil, fmt.Errorf("failed to get tx by signature: %w", err)
+	}
+	if len(response.Meta.InnerInstructions) == 0 {
+		return nil, fmt.Errorf("response.Meta.InnerInstructions is empty")
+	}
+	if len(response.Meta.InnerInstructions[0].Instructions) == 0 {
+		return nil, fmt.Errorf("response.Meta.InnerInstructions[0].Instructions is empty")
 	}
 
-	// Check preconditions
-	//
-	// The transaction data structure uses lots of parallel arrays.
-	// Here we're making sure that we won't get an error due to an index being out of bounds.
-	if len(tx.Meta.PostBalances) != len(tx.Meta.PreBalances) {
-		return nil, fmt.Errorf("encountered mismatching sizes in pre/post balance arrays")
+	// populate the response object
+	txDetail := TxDetail{
+		Timestamp: time.Unix(response.BlockTime, 0).UTC(),
 	}
-	for i := range tx.Meta.PreTokenBalances {
-		if tx.Meta.PreTokenBalances[i].AccountIndex != tx.Meta.PostTokenBalances[i].AccountIndex {
-			return nil, fmt.Errorf("mismatching account indexes")
-		}
-		if tx.Meta.PreTokenBalances[i].AccountIndex >= uint64(len(tx.Transaction.Message.Accounts)) {
-			return nil, fmt.Errorf("pre-token balance index out of range")
-		}
-		if tx.Meta.PostTokenBalances[i].AccountIndex >= uint64(len(tx.Transaction.Message.Accounts)) {
-			return nil, fmt.Errorf("post-token balance index out of range")
-		}
-	}
-
-	// Initialize the struct containing resuts
-	var txDetail TxDetail
-	if tx.BlockTime != nil {
-		txDetail.Timestamp = time.Unix(*tx.BlockTime, 0)
-	}
-
-	// Iterate through balances changes to find the funds source and destination.
-	var receiverFound, senderFound bool
-	for i := range tx.Meta.PreTokenBalances {
-
-		// Convert string balances to big integers
-		var pre, post big.Int
-		_, ok1 := pre.SetString(tx.Meta.PreTokenBalances[i].UITokenAmount.Amount, 10)
-		_, ok2 := post.SetString(tx.Meta.PostTokenBalances[i].UITokenAmount.Amount, 10)
-		if !ok1 || !ok2 {
-			return nil, fmt.Errorf("failed to convert token balances to integers")
-		}
-
-		idx := tx.Meta.PreTokenBalances[i].AccountIndex
-
-		// Did the account's balance increase?
-		if pre.Cmp(&post) == -1 { // pre < post
-			if receiverFound {
-				return nil, fmt.Errorf("unable to identify token receiver in txHash=%s", txHash)
-			}
-			receiverFound = true
-
-			txDetail.Destination = tx.Transaction.Message.Accounts[idx].ToBase58()
-		}
-
-		// Did the account's balance decrease?
-		if pre.Cmp(&post) == 1 { // pre > post
-			if senderFound {
-				return nil, fmt.Errorf("unable to identify token sender in txHash=%s", txHash)
-			}
-			senderFound = true
-
-			txDetail.Source = tx.Transaction.Message.Accounts[idx].ToBase58()
-		}
-	}
-	if !senderFound {
-		return nil, fmt.Errorf("unable to identify participating addresses in txHash=%s", txHash)
+	instruction := response.Meta.InnerInstructions[0].Instructions[0]
+	switch instruction.ParsedInstruction.Type_ {
+	case "transfer":
+		txDetail.Source = instruction.ParsedInstruction.Info.Source
+		txDetail.Destination = instruction.ParsedInstruction.Info.Destination
+	case "burn":
+		txDetail.Source = instruction.ParsedInstruction.Info.Account
+	case "approve":
+		txDetail.Source = instruction.ParsedInstruction.Info.Source
+	default:
+		return nil, fmt.Errorf("unknown parsed instruction type: %s", instruction.ParsedInstruction.Type_)
 	}
 
 	return &txDetail, nil
