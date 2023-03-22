219ee39273988629e94710ac7be646a30772fb59
Agustin Godnic <agustin@xlabs.xyz>
2023-03-22 11:46:17 -0300

    Fix governor endpoints
    A few governor endpoints were handling addresses incorrectly,
    which resulted in errors.
    * `GET /api/v1/governor/config/{governor_address}`
    * `GET /api/v1/governor/status/{governor_address}`
    
    This commit fixes the issue and adds doc comments to prevent the
    same caveat from happening again in the future.

--- api/routes/wormscan/governor/controller.go
+++ api/routes/wormscan/governor/controller.go
@@ -65,7 +65,7 @@ func (c *Controller) FindGovernorConfigurationByGuardianAddress(ctx *fiber.Ctx) 
 	}
 
 	// query the database
-	govConfigs, err := c.srv.FindGovernorConfigByGuardianAddress(ctx.Context(), guardianAddress.Hex())
+	govConfigs, err := c.srv.FindGovernorConfigByGuardianAddress(ctx.Context(), guardianAddress)
 	if err != nil {
 		return err
 	} else if len(govConfigs) == 0 {
@@ -127,7 +127,7 @@ func (c *Controller) FindGovernorStatusByGuardianAddress(ctx *fiber.Ctx) error {
 		return err
 	}
 
-	govStatus, err := c.srv.FindGovernorStatusByGuardianAddress(ctx.Context(), guardianAddress.Hex(), p)
+	govStatus, err := c.srv.FindGovernorStatusByGuardianAddress(ctx.Context(), guardianAddress, p)
 	if err != nil {
 		return err
 	}
