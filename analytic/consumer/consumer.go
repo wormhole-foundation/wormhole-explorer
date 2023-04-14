2028277b30bf259fe507cd9ee5c1a299eb1ccd68
walker-16 <agpazos85@gmail.com>
2023-04-14 12:11:47 -0300

    Avoid process pyth message in analytic (#235)

--- analytic/consumer/consumer.go
+++ analytic/consumer/consumer.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/wormhole-foundation/wormhole-explorer/analytic/metric"
 	"github.com/wormhole-foundation/wormhole-explorer/analytic/queue"
+	"github.com/wormhole-foundation/wormhole-explorer/common/domain"
 	"github.com/wormhole-foundation/wormhole/sdk/vaa"
+	vaa_sdk "github.com/wormhole-foundation/wormhole/sdk/vaa"
 	"go.uber.org/zap"
 )
 
@@ -14,11 +16,12 @@ type Consumer struct {
 	consume    queue.VAAConsumeFunc
 	pushMetric metric.MetricPushFunc
 	logger     *zap.Logger
+	p2pNetwork string
 }
 
 // New creates a new vaa consumer.
-func New(consume queue.VAAConsumeFunc, pushMetric metric.MetricPushFunc, logger *zap.Logger) *Consumer {
-	return &Consumer{consume: consume, pushMetric: pushMetric, logger: logger}
+func New(consume queue.VAAConsumeFunc, pushMetric metric.MetricPushFunc, logger *zap.Logger, p2pNetwork string) *Consumer {
+	return &Consumer{consume: consume, pushMetric: pushMetric, logger: logger, p2pNetwork: p2pNetwork}
 }
 
 // Start consumes messages from VAA queue, parse and store those messages in a repository.
@@ -39,6 +42,13 @@ func (c *Consumer) Start(ctx context.Context) {
 			if err != nil {
 				c.logger.Error("Invalid vaa", zap.String("id", event.ID), zap.Error(err))
 				msg.Failed()
+				continue
+			}
+
+			// filter vaa from pythnet.
+			if c.p2pNetwork == domain.P2pMainNet && vaa_sdk.ChainIDPythNet == vaa.EmitterChain {
+				c.logger.Debug("Skip vaa from pythnet", zap.String("id", event.ID))
+				msg.Done()
 				continue
 			}
 
