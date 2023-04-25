dbdb22bbb200432c65396d0ffb76a19c4a67665a
gipsh <gipsh@gmail.com>
2023-04-25 09:28:44 -0300

    remove 0x prefix and lowercase the hash

--- fly/cmd/backfiller/txhash_strategy.go
+++ fly/cmd/backfiller/txhash_strategy.go
@@ -22,13 +22,24 @@ func workerTxHash(ctx context.Context, repo *storage.Repository, line string) er
 		return fmt.Errorf("error parsing chain id: %v\n", err)
 	}
 
+	//remove 0x from txhash
+	if len(tokens[3]) < 3 {
+		return fmt.Errorf("invalid txhash: %s", tokens[3])
+	}
+	// if token starts with 0x remove it
+	if tokens[3][:2] == "0x" {
+		tokens[3] = tokens[3][2:]
+	}
+
+	txHash := strings.ToLower(tokens[3])
+
 	now := time.Now()
 
 	vaaTxHash := storage.VaaIdTxHashUpdate{
 		ChainID:   vaa.ChainID(intChainID),
 		Emitter:   tokens[1],
 		Sequence:  tokens[2],
-		TxHash:    tokens[3],
+		TxHash:    txHash,
 		UpdatedAt: &now,
 	}
 
