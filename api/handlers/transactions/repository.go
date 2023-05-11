6065f3624d71401e9e5897dc8591d50a68472d77
Agustin Godnic <agustin@xlabs.xyz>
2023-05-11 11:13:08 -0300

    Update queries due to bucket changes

--- api/handlers/transactions/repository.go
+++ api/handlers/transactions/repository.go
@@ -124,7 +124,7 @@ func NewRepository(
 func (r *Repository) GetTopAssetsByVolume(ctx context.Context, timerange *TopAssetsTimerange) ([]AssetDTO, error) {
 
 	// Submit the query to InfluxDB
-	query := fmt.Sprintf(queryTemplateTopAssetsByVolume, r.bucket30DaysRetention, *timerange, r.bucket24HoursRetention)
+	query := fmt.Sprintf(queryTemplateTopAssetsByVolume, r.bucket30DaysRetention, *timerange, r.bucket30DaysRetention)
 	result, err := r.queryAPI.Query(ctx, query)
 	if err != nil {
 		return nil, err
