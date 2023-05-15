c2b94f6448aee92d2a1404e0d6d0f4949e68ae8e
ftocal <46001274+ftocal@users.noreply.github.com>
2023-05-15 15:15:12 -0300

    Add total tx count and total tx volume by portal bridge for scorecards (#314)
    Co-authored-by: walker-16 <agpazos85@gmail.com>

--- api/handlers/transactions/queries.go
+++ api/handlers/transactions/queries.go
@@ -64,3 +64,45 @@ func createRangeQuery(t time.Time, timeSpan string) (string, string) {
 
 	return startLastVaa.Format(format), startAggregatesVaa.Format(format)
 }
+
+const queryTemplateTotalTrxCount = `
+current = from(bucket: "%s")
+  |> range(start: %s)
+  |> filter(fn: (r) => r["_measurement"] == "vaa_volume")
+  |> filter(fn: (r) => r["_field"] == "volume")
+  |> group()
+  |> count()
+last = from(bucket: "%s")
+  |> range(start: -1mo)
+  |> filter(fn: (r) => r["_measurement"] == "total_tx_count")
+  |> last()
+union(tables: [current, last])
+  |> group()
+  |> sum()
+`
+
+func buildTotalTrxCountQuery(bucketForever, bucket30Days string, t time.Time) string {
+	start := t.Truncate(time.Hour * 24).Format(time.RFC3339Nano)
+	return fmt.Sprintf(queryTemplateTotalTrxCount, bucketForever, start, bucket30Days)
+}
+
+const queryTemplateTotalTrxVolume = `
+current = from(bucket: "%s")
+  |> range(start: %s)
+  |> filter(fn: (r) => r["_measurement"] == "vaa_volume")
+  |> filter(fn: (r) => r["_field"] == "volume")
+  |> group()
+  |> sum()
+last = from(bucket: "%s")
+  |> range(start: -1mo)
+  |> filter(fn: (r) => r["_measurement"] == "total_tx_volume")
+  |> last()
+union(tables: [current, last])
+  |> group()
+  |> sum()
+`
+
+func buildTotalTrxVolumeQuery(bucketForever, bucket30Days string, t time.Time) string {
+	start := t.Truncate(time.Hour * 24).Format(time.RFC3339Nano)
+	return fmt.Sprintf(queryTemplateTotalTrxVolume, bucketForever, start, bucket30Days)
+}
