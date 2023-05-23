fd45d763b20fde999ff902f5ab28d154520a0b44
Agustin Godnic <agustin@xlabs.xyz>
2023-05-23 10:32:37 -0300

    Move helper function to separate file

--- influx-backfiller/cmd/metrics/vaa_count.go
+++ influx-backfiller/cmd/metrics/vaa_count.go
@@ -2,11 +2,8 @@ package metrics
 
 import (
 	"context"
-	"fmt"
 	"os"
-	"strings"
 
-	"github.com/influxdata/influxdb-client-go/v2/api/write"
 	"github.com/wormhole-foundation/wormhole-explorer/analytic/metric"
 	sdk "github.com/wormhole-foundation/wormhole/sdk/vaa"
 	"github.com/xlabs/influx-backfiller/parser"
@@ -46,29 +43,4 @@ func RunVaaCount(inputFile, outputFile string) {
 	csvParser := parser.NewVaaCsvParser(processorFunc, inputFile)
 
 	csvParser.Start(context.Background())
-}
-
-// convertPointToLineProtocol transforms a given data point into the format that InfluxDB uses for dumps.
-//
-// See https://docs.influxdata.com/influxdb/v2.0/reference/syntax/line-protocol/
-func convertPointToLineProtocol(point *write.Point) string {
-
-	// Collect tags
-	var tags string
-	for _, t := range point.TagList() {
-		tags += fmt.Sprintf(",%s=%s", t.Key, t.Value)
-	}
-
-	// Collect fields
-	if len(point.FieldList()) == 0 {
-		panic("expected at least one point in metric")
-	}
-	var tmp []string
-	for _, f := range point.FieldList() {
-		tmp = append(tmp, fmt.Sprintf("%s=%v", f.Key, f.Value))
-	}
-	fields := strings.Join(tmp, ",")
-
-	// Build a line for the dump file
-	return fmt.Sprintf("%s%s %s %d\n", point.Name(), tags, fields, point.Time().UnixNano())
 }
