2930da5c3f9bb294a9c9cc7f0bd8a4d4ef651e3e
Agustin Pazos <agpazos85@gmail.com>
2023-06-27 12:21:46 -0300

    remove legacy web project

--- web/src/utils/chainIdToName.ts
+++
@@ -1,8 +0,0 @@
-import { CHAINS } from "@certusone/wormhole-sdk";
-
-const chainIdToNameMap = Object.fromEntries(
-  Object.entries(CHAINS).map(([key, value]) => [value, key])
-);
-const chainIdToName = (chainId: number) =>
-  chainIdToNameMap[chainId] || "Unknown";
-export default chainIdToName;
--- web/src/utils/getGovernorTokenList.ts
+++
@@ -1,9 +0,0 @@
-import { publicrpc } from "@certusone/wormhole-sdk-proto-web";
-import { Network } from "../contexts/NetworkContext";
-const { GrpcWebImpl, PublicRPCServiceClientImpl } = publicrpc;
-
-export async function getGovernorTokenList(network: Network) {
-  const rpc = new GrpcWebImpl(network.endpoint, {});
-  const api = new PublicRPCServiceClientImpl(rpc);
-  return await api.GovernorGetTokenList({});
-}
