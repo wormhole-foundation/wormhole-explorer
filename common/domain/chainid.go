a5ebd060edb7316c379312e879d3aa790475d2ba
Agustin Godnic <agustin@xlabs.xyz>
2023-06-28 11:14:01 -0300

    Style fixes

--- common/domain/chainid.go
+++ common/domain/chainid.go
@@ -11,84 +11,25 @@ import (
 	sdk "github.com/wormhole-foundation/wormhole/sdk/vaa"
 )
 
-// GetSupportedChainIDs returns a map of all supported chain IDs to their respective names.
-func GetSupportedChainIDs() map[sdk.ChainID]string {
-	chainIDs := sdk.GetAllNetworkIDs()
-	supportedChaindIDs := make(map[sdk.ChainID]string, len(chainIDs))
-	for _, chainID := range chainIDs {
-		supportedChaindIDs[chainID] = chainID.String()
+var (
+	// nearKnownEmitters maps NEAR emitter addresses to NEAR accounts.
+	nearKnownEmitters = map[string]string{
+		"148410499d3fcda4dcfd68a1ebfcdddda16ab28326448d4aae4d2f0465cdfcb7": "contract.portalbridge.near",
 	}
-	return supportedChaindIDs
-}
 
-// EncodeTrxHashByChainID encodes the transaction hash by chain id with different encoding methods.
-func EncodeTrxHashByChainID(chainID sdk.ChainID, txHash []byte) (string, error) {
-	switch chainID {
-	case sdk.ChainIDSolana:
-		return base58.Encode(txHash), nil
-	case sdk.ChainIDEthereum:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDTerra:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDBSC:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDPolygon:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDAvalanche:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDOasis:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDAlgorand:
-		return base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(txHash), nil
-	case sdk.ChainIDAurora:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDFantom:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDKarura:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDAcala:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDKlaytn:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDCelo:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDNear:
-		return base58.Encode(txHash), nil
-	case sdk.ChainIDMoonbeam:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDNeon:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDTerra2:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDInjective:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDSui:
-		return base58.Encode(txHash), nil
-	case sdk.ChainIDAptos:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDArbitrum:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDOptimism:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDXpla:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDBtc:
-		//TODO: check if this is correct
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDBase:
-		//TODO: check if this is correct
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDSei:
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDWormchain:
-		//TODO: check if this is correct
-		return hex.EncodeToString(txHash), nil
-	case sdk.ChainIDSepolia:
-		return hex.EncodeToString(txHash), nil
-	default:
-		return hex.EncodeToString(txHash), fmt.Errorf("unknown chain id: %d", chainID)
+	// suiKnownEmitters maps Sui emitter addresses to Sui accounts.
+	suiKnownEmitters = map[string]string{
+		"ccceeb29348f71bdd22ffef43a2a19c1f5b5e17c5cca5411529120182672ade5": "0xc57508ee0d4595e5a8728974a4a93a787d38f339757230d441e895422c07aba9",
 	}
-}
+
+	// aptosKnownEmitters maps Aptos emitter addresses to Aptos accounts.
+	aptosKnownEmitters = map[string]string{
+		// Token Bridge
+		"0000000000000000000000000000000000000000000000000000000000000001": "0x576410486a2da45eee6c949c995670112ddf2fbeedab20350d506328eefc9d4f",
+		// NFT Bridge
+		"0000000000000000000000000000000000000000000000000000000000000005": "0x1bdffae984043833ed7fe223f7af7a3f8902d04129b14f801823e64827da7130",
+	}
+)
 
 // TranslateEmitterAddress converts an emitter address into the corresponding native address for the given chain.
 func TranslateEmitterAddress(chainID sdk.ChainID, address string) (string, error) {
@@ -171,21 +112,21 @@ func TranslateEmitterAddress(chainID sdk.ChainID, address string) (string, error
 		return addr.String(), nil
 
 	case sdk.ChainIDNear:
-		if nativeAddress, ok := nearMappings[address]; ok {
+		if nativeAddress, ok := nearKnownEmitters[address]; ok {
 			return nativeAddress, nil
 		} else {
 			return "", fmt.Errorf(`no mapping found for NEAR emitter address "%s"`, address)
 		}
 
 	case sdk.ChainIDSui:
-		if nativeAddress, ok := suiMappings[address]; ok {
+		if nativeAddress, ok := suiKnownEmitters[address]; ok {
 			return nativeAddress, nil
 		} else {
 			return "", fmt.Errorf(`no mapping found for Sui emitter address "%s"`, address)
 		}
 
 	case sdk.ChainIDAptos:
-		if nativeAddress, ok := aptosMappings[address]; ok {
+		if nativeAddress, ok := aptosKnownEmitters[address]; ok {
 			return nativeAddress, nil
 		} else {
 			return "", fmt.Errorf(`no mapping found for Aptos emitter address "%s"`, address)
@@ -196,19 +137,81 @@ func TranslateEmitterAddress(chainID sdk.ChainID, address string) (string, error
 	}
 }
 
-var nearMappings = map[string]string{
-	"148410499d3fcda4dcfd68a1ebfcdddda16ab28326448d4aae4d2f0465cdfcb7": "contract.portalbridge.near",
+// GetSupportedChainIDs returns a map of all supported chain IDs to their respective names.
+func GetSupportedChainIDs() map[sdk.ChainID]string {
+	chainIDs := sdk.GetAllNetworkIDs()
+	supportedChaindIDs := make(map[sdk.ChainID]string, len(chainIDs))
+	for _, chainID := range chainIDs {
+		supportedChaindIDs[chainID] = chainID.String()
+	}
+	return supportedChaindIDs
 }
 
-var suiMappings = map[string]string{
-	"ccceeb29348f71bdd22ffef43a2a19c1f5b5e17c5cca5411529120182672ade5": "0xc57508ee0d4595e5a8728974a4a93a787d38f339757230d441e895422c07aba9",
-}
-
-var aptosMappings = map[string]string{
-
-	// Token Bridge
-	"0000000000000000000000000000000000000000000000000000000000000001": "0x576410486a2da45eee6c949c995670112ddf2fbeedab20350d506328eefc9d4f",
-
-	// NFT Bridge
-	"0000000000000000000000000000000000000000000000000000000000000005": "0x1bdffae984043833ed7fe223f7af7a3f8902d04129b14f801823e64827da7130",
+// EncodeTrxHashByChainID encodes the transaction hash by chain id with different encoding methods.
+func EncodeTrxHashByChainID(chainID sdk.ChainID, txHash []byte) (string, error) {
+	switch chainID {
+	case sdk.ChainIDSolana:
+		return base58.Encode(txHash), nil
+	case sdk.ChainIDEthereum:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDTerra:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDBSC:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDPolygon:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDAvalanche:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDOasis:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDAlgorand:
+		return base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(txHash), nil
+	case sdk.ChainIDAurora:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDFantom:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDKarura:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDAcala:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDKlaytn:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDCelo:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDNear:
+		return base58.Encode(txHash), nil
+	case sdk.ChainIDMoonbeam:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDNeon:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDTerra2:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDInjective:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDSui:
+		return base58.Encode(txHash), nil
+	case sdk.ChainIDAptos:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDArbitrum:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDOptimism:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDXpla:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDBtc:
+		//TODO: check if this is correct
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDBase:
+		//TODO: check if this is correct
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDSei:
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDWormchain:
+		//TODO: check if this is correct
+		return hex.EncodeToString(txHash), nil
+	case sdk.ChainIDSepolia:
+		return hex.EncodeToString(txHash), nil
+	default:
+		return hex.EncodeToString(txHash), fmt.Errorf("unknown chain id: %d", chainID)
+	}
 }
