9600ed7cfff55d92b3aa2e37734fdaaf8d877473
Agustin Pazos <agpazos85@gmail.com>
2023-07-04 10:09:11 -0300

    Add opsgenie alertClient to parser

---
+++ parser/internal/alert/alert.go
@@ -0,0 +1,8 @@
+package alert
+
+import "github.com/wormhole-foundation/wormhole-explorer/common/client/alert"
+
+func LoadAlerts(cfg alert.AlertConfig) map[string]alert.Alert {
+	alerts := make(map[string]alert.Alert)
+	return alerts
+}
