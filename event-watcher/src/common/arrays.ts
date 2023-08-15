9744e8486bda7aafd1873576a7d14f4e248e4f36
gipsh <gipshm@gmail.com>
2023-08-15 19:06:10 -0300

    fix commons

---
+++ event-watcher/src/common/arrays.ts
@@ -0,0 +1,7 @@
+export function chunkArray<T>(arr: T[], size: number): T[][] {
+  const chunks = [];
+  for (let i = 0; i < arr.length; i += size) {
+    chunks.push(arr.slice(i, i + size));
+  }
+  return chunks;
+}
---
+++ event-watcher/src/common/index.ts
@@ -0,0 +1,3 @@
+export * from './arrays';
+export * from './consts';
+export * from './utils';
