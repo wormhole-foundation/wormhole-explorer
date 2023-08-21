af89301b2abf9bf686688dd4843864c9b43bfe44
Fernando Torres <fert1335@gmail.com>
2023-08-21 16:58:16 -0300

    Handle new type of messages from fly and event-watcher in tx-tracker

--- tx-tracker/queue/queue.go
+++ tx-tracker/queue/queue.go
@@ -12,25 +12,20 @@ type sqsEvent struct {
 	Message   string `json:"Message"`
 }
 
-// VaaEvent represents a vaa data to be handle by the pipeline.
-type VaaEvent struct {
-	ID               string      `json:"id"`
-	ChainID          sdk.ChainID `json:"emitterChain"`
-	EmitterAddress   string      `json:"emitterAddr"`
-	Sequence         string      `json:"sequence"`
-	GuardianSetIndex uint32      `json:"guardianSetIndex"`
-	Vaa              []byte      `json:"vaas"`
-	IndexedAt        time.Time   `json:"indexedAt"`
-	Timestamp        *time.Time  `json:"timestamp"`
-	UpdatedAt        *time.Time  `json:"updatedAt"`
-	TxHash           string      `json:"txHash"`
-	Version          uint16      `json:"version"`
-	Revision         uint16      `json:"revision"`
+// Event represents a vaa data to be handle by the pipeline.
+type Event struct {
+	ID             string
+	ChainID        sdk.ChainID
+	EmitterAddress string
+	Sequence       string
+	Vaa            []byte
+	Timestamp      *time.Time
+	TxHash         string
 }
 
 // ConsumerMessage defition.
 type ConsumerMessage interface {
-	Data() *VaaEvent
+	Data() *Event
 	Done()
 	Failed()
 	IsExpired() bool
