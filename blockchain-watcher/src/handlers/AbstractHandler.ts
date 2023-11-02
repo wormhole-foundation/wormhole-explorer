57b7940a12dc38515dd2f20bf754187f4ea6d893
chase-45 <chasemoran45@gmail.com>
2023-11-01 21:59:32 -0400

    modifying event handler interface

--- blockchain-watcher/src/handlers/AbstractHandler.ts
+++ blockchain-watcher/src/handlers/AbstractHandler.ts
@@ -1,37 +1,65 @@
 import { ChainId, Network } from "@certusone/wormhole-sdk";
+import { v4 as uuidv4 } from "uuid";
+const { createHash } = require("crypto");
 
-export default abstract class EventHandler<T> {
+type SyntheticEvent = {
+  eventName: string;
+  eventVersion: number;
+  eventChain: ChainId;
+  observationTimestamp: number;
+  uuid: string; //UUID for the event, good for deduping
+  dataHash: string; //sha256 hash of the event data, good for deduping
+  data: any;
+};
+
+export default abstract class EventHandler {
   public name: string;
 
   constructor(name: string) {
     this.name = name;
   }
 
-  public abstract getEventSignatureEvm(): string | null;
-  public abstract getEventAbiEvm(): string[] | null;
-  public abstract handleEventEvm(
-    chainId: ChainId,
-    ...args: any
-  ): Promise<T | null>;
+  //These top level functions must always be implemented
   public abstract shouldSupportChain(
     network: Network,
     chainId: ChainId
   ): boolean;
-  public abstract persistRecord(record: T): Promise<void>;
+
+  //These functions must be implemented if an EVM chain is supported.
+
+  //Event to be listened for in ABI format. Example:
+  //"event Delivery(address indexed recipientContract, uint16 indexed sourceChain, uint64 indexed sequence, bytes32 deliveryVaaHash, uint8 status, uint256 gasUsed, uint8 refundStatus, bytes additionalStatusInfo, bytes overridesInfo)",
+  public abstract getEventAbiEvm(): string[] | null;
+
+  //Event to be listened for in signature format. Example:
+  //"Delivery(address,uint16,uint64,bytes32,uint8,uint256,uint8,bytes,bytes)"
+  public abstract getEventSignatureEvm(): string | null;
+
+  //This function will be called when a subscribed event is received from the ethers provider.
+  //TODO pretty sure the ...args is always an ethers.Event object
+  public abstract handleEventEvm(
+    chainId: ChainId,
+    ...args: any
+  ): Promise<SyntheticEvent[]>;
   public abstract getContractAddressEvm(
     network: Network,
     chainId: ChainId
   ): string;
 
-  public getEventListener(handler: EventHandler<any>, chainId: ChainId) {
+  //*** Non-abstract functions
+
+  //Wrapper function to hand into EVM rpc provider.
+  //The wrapper is necessary otherwise we can't figure out which chain ID the event came from.
+  public getEventListener(handler: EventHandler, chainId: ChainId) {
     //@ts-ignore
     return (...args) => {
       // @ts-ignore
       return handler
         .handleEventEvm(chainId, ...args)
-        .then((record) => {
-          if (record) {
-            handler.persistRecord(record);
+        .then((records) => {
+          if (records) {
+            //TODO persist records. Unsure how exactly this happens atm.
+            //handler.persistRecord(record);
           }
         })
         .catch((e) => {
@@ -42,6 +70,30 @@ export default abstract class EventHandler<T> {
           );
           console.error(e);
         });
+    };
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public generateUuid(): string {
+    return uuidv4();
+  }
+
+  private wrapEvent(
+    chainId: ChainId,
+    version: number,
+    data: any
+  ): SyntheticEvent {
+    return {
+      eventName: this.name,
+      eventVersion: version,
+      eventChain: chainId,
+      observationTimestamp: Date.now(),
+      uuid: this.generateUuid(),
+      dataHash: createHash("sha256").update(JSON.stringify(data)).digest("hex"),
+      data: data,
     };
   }
 }
