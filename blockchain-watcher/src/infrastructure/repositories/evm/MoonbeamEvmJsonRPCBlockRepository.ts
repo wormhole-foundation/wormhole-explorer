f2b684f2c47ca43b7f7918974a4910c3736a95f4
JulianMerlo <julimerlo95@gmail.com>
2023-12-11 19:29:22 -0300

    Define variables inside the object

--- blockchain-watcher/src/infrastructure/repositories/evm/MoonbeamEvmJsonRPCBlockRepository.ts
+++ blockchain-watcher/src/infrastructure/repositories/evm/MoonbeamEvmJsonRPCBlockRepository.ts
@@ -8,12 +8,12 @@ import {
 
 const GROW_SLEEP_TIME = 50;
 const MAX_ATTEMPTS = 20;
-let isBlockFinalized = false;
-let sleepTime = 100;
-let attempts = 0;
 
 export class MoonbeamEvmJsonRPCBlockRepository extends EvmJsonRPCBlockRepository {
   override readonly logger = winston.child({ module: "MoonbeamEvmJsonRPCBlockRepository" });
+  private isBlockFinalized = false;
+  private sleepTime = 100;
+  private attempts = 0;
 
   constructor(cfg: EvmJsonRPCBlockRepositoryCfg, httpClient: HttpClient) {
     super(cfg, httpClient);
@@ -23,7 +23,7 @@ export class MoonbeamEvmJsonRPCBlockRepository extends EvmJsonRPCBlockRepository
     const chainCfg = this.getCurrentChain(chain);
     const blockNumber: bigint = await super.getBlockHeight(chain, finality);
 
-    while (!isBlockFinalized && attempts < MAX_ATTEMPTS) {
+    while (!this.isBlockFinalized && this.attempts < MAX_ATTEMPTS) {
       try {
         this.sleep();
 
@@ -40,23 +40,23 @@ export class MoonbeamEvmJsonRPCBlockRepository extends EvmJsonRPCBlockRepository
           { timeout: chainCfg.timeout, retries: chainCfg.retries }
         );
 
-        isBlockFinalized = result ?? false;
-        attempts++;
+        this.isBlockFinalized = result ?? false;
+        this.attempts++;
       } catch (e) {
         this.handleError(chain, e, "getBlockHeight", "eth_getBlockByNumber");
-        attempts++;
+        this.attempts++;
       }
     }
 
-    if (attempts > MAX_ATTEMPTS)
+    if (this.attempts > MAX_ATTEMPTS)
       this.logger.error(`[getBlockHeight] The block ${blockNumber} never ended`);
 
     return blockNumber;
   }
 
   private sleep() {
-    sleepTime = sleepTime + GROW_SLEEP_TIME;
-    return new Promise((resolve) => setTimeout(resolve, sleepTime));
+    this.sleepTime = this.sleepTime + GROW_SLEEP_TIME;
+    return new Promise((resolve) => setTimeout(resolve, this.sleepTime));
   }
 }
 
