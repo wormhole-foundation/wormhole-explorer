a4bf43c17e46b7bb8eff2054ca422116b47d0a94
Julian <52217955+julianmerlo95@users.noreply.github.com>
2023-12-12 10:27:30 -0300

    [Blockchain Watcher] (EVM) Add moonbeam chain (#886)
    * feature-826/enable-evm-arbitrum-chain
    
    * Improve arbitrum implementation
    
    * Add removeDuplicates method and divide blocks into testnet and mainnet
    
    * Delete findAssociatedL1Block method
    
    * feature-826/enable-evm-moonbeam-chain
    
    * Improve result
    
    * Add max attempts
    
    * Add job
    
    * Mapped in custom-env
    
    * Add test
    
    * Add configMock
    
    * Resolve comment in PR
    
    * Resolve comment in PR
    
    * Resolve comment in PR
    
    * Resolve test
    
    * Define variables inside the object
    
    * change interval value in testnet
    
    * Add equal attempts to maxAttempts
    
    * Add timers/promises to sleep function
    
    * Run prettier
    
    ---------
    
    Co-authored-by: Julian Merlo <julianmerlo@MacBook-Pro-de-Julian.local>

---
+++ blockchain-watcher/src/infrastructure/repositories/evm/MoonbeamEvmJsonRPCBlockRepository.ts
@@ -0,0 +1,66 @@
+import { HttpClient } from "../../rpc/http/HttpClient";
+import { setTimeout } from "timers/promises";
+import { EvmTag } from "../../../domain/entities";
+import winston from "../../log";
+import {
+  EvmJsonRPCBlockRepository,
+  EvmJsonRPCBlockRepositoryCfg,
+} from "./EvmJsonRPCBlockRepository";
+
+const GROW_SLEEP_TIME = 50;
+const MAX_ATTEMPTS = 20;
+
+export class MoonbeamEvmJsonRPCBlockRepository extends EvmJsonRPCBlockRepository {
+  override readonly logger = winston.child({ module: "MoonbeamEvmJsonRPCBlockRepository" });
+  private isBlockFinalized = false;
+  private sleepTime = 100;
+  private attempts = 0;
+
+  constructor(cfg: EvmJsonRPCBlockRepositoryCfg, httpClient: HttpClient) {
+    super(cfg, httpClient);
+  }
+
+  async getBlockHeight(chain: string, finality: EvmTag): Promise<bigint> {
+    const chainCfg = this.getCurrentChain(chain);
+    const blockNumber: bigint = await super.getBlockHeight(chain, finality);
+
+    while (!this.isBlockFinalized && this.attempts <= MAX_ATTEMPTS) {
+      try {
+        await this.sleep();
+
+        const { hash } = await super.getBlock(chain, blockNumber);
+
+        const { result } = await this.httpClient.post<BlockIsFinalizedResult>(
+          chainCfg.rpc.href,
+          {
+            jsonrpc: "2.0",
+            id: 1,
+            method: "moon_isBlockFinalized",
+            params: [hash],
+          },
+          { timeout: chainCfg.timeout, retries: chainCfg.retries }
+        );
+
+        this.isBlockFinalized = result ?? false;
+        this.attempts++;
+      } catch (e) {
+        this.handleError(chain, e, "getBlockHeight", "eth_getBlockByNumber");
+        this.attempts++;
+      }
+    }
+
+    if (this.attempts > MAX_ATTEMPTS)
+      this.logger.error(`[getBlockHeight] The block ${blockNumber} never ended`);
+
+    return blockNumber;
+  }
+
+  private async sleep() {
+    this.sleepTime = this.sleepTime + GROW_SLEEP_TIME;
+    await setTimeout(this.sleepTime, null, { ref: false });
+  }
+}
+
+type BlockIsFinalizedResult = {
+  result: boolean;
+};
