dfa92fcc799e1be122ab44b9577505f56a2c51fa
M-Picco <martin@xlabs.xyz>
2024-02-06 20:17:40 -0300

    Set up husky and pre commit hook to run prettier

--- blockchain-watcher/test/mocks/mockRpcPool.ts
+++ blockchain-watcher/test/mocks/mockRpcPool.ts
@@ -3,32 +3,40 @@ import axios from "axios";
 
 export class ProviderHealthInstrumentationMock {
   fetch = async (input: string | URL | Request, init?: RequestInit) => {
-    const body = typeof init?.body === 'string' ? JSON.parse(init.body) : init?.body;
+    const body = typeof init?.body === "string" ? JSON.parse(init.body) : init?.body;
     const res = await axios.request({
       url: input.toString(),
-      method: 'POST',
-      data: body
+      method: "POST",
+      data: body,
     });
     return {
       status: 200,
-      json: () => res.data
+      json: () => res.data,
     };
-  }
-}
-
-type RpcConfig = {url:string};
-type PoolSupplier = <T>(cfg: RpcConfig, createProvider: (cfg: RpcConfig) => T, type?: string) => { get: () => T };
-const providerPoolSupplier: PoolSupplier = <T>(cfg: RpcConfig, createProvider: (cfg: RpcConfig) => T, type?: string) => {
-  return {
-    get: () => createProvider(cfg)
   };
 }
 
+type RpcConfig = { url: string };
+type PoolSupplier = <T>(
+  cfg: RpcConfig,
+  createProvider: (cfg: RpcConfig) => T,
+  type?: string
+) => { get: () => T };
+const providerPoolSupplier: PoolSupplier = <T>(
+  cfg: RpcConfig,
+  createProvider: (cfg: RpcConfig) => T,
+  type?: string
+) => {
+  return {
+    get: () => createProvider(cfg),
+  };
+};
+
 export function mockRpcPool() {
-  jest.mock('@xlabs/rpc-pool', () => {
+  jest.mock("@xlabs/rpc-pool", () => {
     return {
       ProviderHealthInstrumentation: ProviderHealthInstrumentationMock,
-      providerPoolSupplier
+      providerPoolSupplier,
     };
   });
 }
