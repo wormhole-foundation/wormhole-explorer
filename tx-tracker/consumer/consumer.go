4228a93f6dd9ec4f672f228bdc026df6b2bf6080
Agustin Pazos <agpazos85@gmail.com>
2024-03-14 12:40:57 -0300

    Enable overwrite to stress test

--- tx-tracker/consumer/consumer.go
+++ tx-tracker/consumer/consumer.go
@@ -100,7 +100,7 @@ func (c *Consumer) processSourceTx(ctx context.Context, msg queue.ConsumerMessag
 		Sequence:  event.Sequence,
 		TxHash:    event.TxHash,
 		Metrics:   c.metrics,
-		Overwrite: false, // avoid processing the same transaction twice
+		Overwrite: true, // avoid processing the same transaction twice
 	}
 	_, err := ProcessSourceTx(ctx, c.logger, c.rpcpool, c.repository, &p, c.p2pNetwork)
 
