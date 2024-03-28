028fc62c47c7ed7e05c121055f9b23ef788bfc7c
Mariano <9205080+marianososto@users.noreply.github.com>
2024-03-28 09:12:42 -0300

    add queryLoggging and remove other parts of the query to troubleshooting

--- api/main.go
+++ api/main.go
@@ -111,7 +111,7 @@ func main() {
 
 	// Setup DB
 	rootLogger.Info("connecting to MongoDB")
-	db, err := dbutil.Connect(appCtx, rootLogger, cfg.DB.URL, cfg.DB.Name, false)
+	db, err := dbutil.Connect(appCtx, rootLogger, cfg.DB.URL, cfg.DB.Name, true)
 	if err != nil {
 		rootLogger.Fatal("failed to connect to MongoDB", zap.Error(err))
 	}
