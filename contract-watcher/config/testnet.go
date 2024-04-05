0658d5a739fb02f985e144038fbf2bd8f305e766
Agustin Pazos <agpazos85@gmail.com>
2024-04-05 11:22:13 -0300

    Fix contract remove

--- contract-watcher/config/testnet.go
+++ contract-watcher/config/testnet.go
@@ -155,3 +155,35 @@ var OPTIMISM_TESTNET = WatcherBlockchainAddresses{
 		},
 	},
 }
+
+var BASE_TESTNET = WatcherBlockchainAddresses{
+	ChainID:      vaa.ChainIDBase,
+	Name:         "base_goerli",
+	SizeBlocks:   100,
+	WaitSeconds:  10,
+	InitialBlock: 902_385,
+	MethodsByAddress: map[string][]BlockchainMethod{
+		strings.ToLower("0xA31aa3FDb7aF7Db93d18DDA4e19F811342EDF780"): {
+			{
+				ID:   MetehodIDCompleteTransferWithRelay,
+				Name: MetehodCompleteTransferWithRelay,
+			},
+		},
+	},
+}
+
+var BASE_SEPOLIA_TESTNET = WatcherBlockchainAddresses{
+	ChainID:      vaa.ChainIDBaseSepolia,
+	Name:         "base_sepolia",
+	SizeBlocks:   100,
+	WaitSeconds:  10,
+	InitialBlock: 3_415_420,
+	MethodsByAddress: map[string][]BlockchainMethod{
+		strings.ToLower("0x86F55A04690fd7815A3D802bD587e83eA888B239"): {
+			{
+				ID:   MetehodIDCompleteTransferWithRelay,
+				Name: MetehodCompleteTransferWithRelay,
+			},
+		},
+	},
+}
