6b67bf859e2f1918bb7cfb15f5e19af932448323
Julian <52217955+julianmerlo95@users.noreply.github.com>
2024-04-15 16:48:56 -0300

    [Blockchain Watcher] (SCROLL) Mapped scroll source and target events (#1304)
    * Mapped scroll source and target events
    
    * Remove jobs for mainnet
    
    * Remove jobs for mainnet
    
    * Mapped redeem contract
    
    * Add aptos custom env
    
    ---------
    
    Co-authored-by: julian merlo <julianmerlo@julians-MacBook-Pro.local>

--- blockchain-watcher/src/infrastructure/repositories/RepositoriesBuilder.ts
+++ blockchain-watcher/src/infrastructure/repositories/RepositoriesBuilder.ts
@@ -53,6 +53,7 @@ const EVM_CHAINS = new Map([
   ["moonbeam", "moonbeam-evmRepo"],
   ["polygon", "polygon-evmRepo"],
   ["ethereum-holesky", "evmRepo"],
+  ["scroll", "evmRepo"],
 ]);
 
 const POOL_STRATEGY = "weighted";
