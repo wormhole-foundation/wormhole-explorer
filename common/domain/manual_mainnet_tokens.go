fd3d6f81c9e0739cc5399fdf6c54e3df549901d9
Fernando Torres <fert1335@gmail.com>
2024-05-14 11:08:31 -0300

    Add BORG token to the token list for ethereum and solana
    Co-authored-by: walker-16 <agpazos85@gmail.com>

--- common/domain/manual_mainnet_tokens.go
+++ common/domain/manual_mainnet_tokens.go
@@ -3,11 +3,13 @@ package domain
 // manualMainnetTokenList returns a list of tokens that are not generated automatically.
 func manualMainnetTokenList() []TokenMetadata {
 	return []TokenMetadata{
-		{TokenChain: 1, TokenAddress: "6927fdc01ea906f96d7137874cdd7adad00ca35764619310e54196c781d84d5b", Symbol: "W", CoingeckoID: "wormhole", Decimals: 6},   // Addr: 85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ
 		{TokenChain: 2, TokenAddress: "000000000000000000000000b0ffa8000886e57f86dd5264b9582b2ad87b2b91", Symbol: "W", CoingeckoID: "wormhole", Decimals: 18},  // Addr: 0xB0fFa8000886e57F86dd5264b9582b2Ad87b2b91
 		{TokenChain: 23, TokenAddress: "000000000000000000000000b0ffa8000886e57f86dd5264b9582b2ad87b2b91", Symbol: "W", CoingeckoID: "wormhole", Decimals: 18}, // Addr: 0xB0fFa8000886e57F86dd5264b9582b2Ad87b2b91
 		{TokenChain: 24, TokenAddress: "000000000000000000000000b0ffa8000886e57f86dd5264b9582b2ad87b2b91", Symbol: "W", CoingeckoID: "wormhole", Decimals: 18}, // Addr: 0xB0fFa8000886e57F86dd5264b9582b2Ad87b2b91
 		{TokenChain: 30, TokenAddress: "000000000000000000000000b0ffa8000886e57f86dd5264b9582b2ad87b2b91", Symbol: "W", CoingeckoID: "wormhole", Decimals: 18}, // Addr: 0xB0fFa8000886e57F86dd5264b9582b2Ad87b2b91
+
+		{TokenChain: 1, TokenAddress: "270ad0028e970df757d5f14f8cbb6a6810e48139125608ea958b718eb2944920", Symbol: "BORG", CoingeckoID: "swissborg", Decimals: 9},  // Addr: 3dQTr7ror2QPKQ3GbBCokJUmjErGg8kTJzdnYjNfvi3Z
+		{TokenChain: 2, TokenAddress: "00000000000000000000000064d0f55cd8c7133a9d7102b13987235f486f2224", Symbol: "BORG", CoingeckoID: "swissborg", Decimals: 18}, // Addr: 0x64d0f55cd8c7133a9d7102b13987235f486f2224
 
 	}
 }
