79dfff688d5f76ba285eb7ca96b4ee3496048c5a
Mariano <9205080+marianososto@users.noreply.github.com>
2024-05-29 12:14:31 -0300

    revert changes

--- analytics/cmd/main.go
+++ analytics/cmd/main.go
@@ -140,28 +140,6 @@ func addVaaVolumeFromMongoCommand(parent *cobra.Command) {
 
 }
 
-func addVaaVolumeV3FromParsedVaaCollection(parent *cobra.Command) {
-	var mongoUri, mongoDb, output string
-	//vaa-volume from MongoDB
-	vaaVolumeMongoCmd := &cobra.Command{
-		Use:   "mongo-parsedvaa",
-		Short: "Generate volume metrics from MongoDB - ParsedVaa collection",
-		Run: func(_ *cobra.Command, _ []string) {
-			metrics.RunVaaVolumeV3BackFillerFromMongo(mongoUri, mongoDb, output)
-		},
-	}
-
-	//mongo flags
-	vaaVolumeMongoCmd.Flags().StringVar(&mongoUri, "mongo-uri", "", "Mongo connection")
-	vaaVolumeMongoCmd.Flags().StringVar(&mongoDb, "mongo-database", "", "Mongo database")
-
-	// output flag
-	vaaVolumeMongoCmd.Flags().StringVar(&output, "output", "", "path to output file")
-	vaaVolumeMongoCmd.MarkFlagRequired("output")
-
-	parent.AddCommand(vaaVolumeMongoCmd)
-}
-
 func addVaaVolumeCommand(parent *cobra.Command) {
 
 	vaaVolumeCmd := &cobra.Command{
@@ -171,7 +149,6 @@ func addVaaVolumeCommand(parent *cobra.Command) {
 
 	addVaaVolumeFromFileCommand(vaaVolumeCmd)
 	addVaaVolumeFromMongoCommand(vaaVolumeCmd)
-	addVaaVolumeV3FromParsedVaaCollection(vaaVolumeCmd)
 	parent.AddCommand(vaaVolumeCmd)
 }
 
