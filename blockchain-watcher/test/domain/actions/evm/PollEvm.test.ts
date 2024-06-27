156f82640778077348dceca0515418d5dffdf7f6
julian merlo <julianmerlo@julians-MacBook-Pro-2.local>
2024-06-27 16:22:20 -0300

    Resolve evm test

--- blockchain-watcher/test/domain/actions/evm/PollEvm.test.ts
+++ blockchain-watcher/test/domain/actions/evm/PollEvm.test.ts
@@ -33,6 +33,7 @@ describe("PollEvm", () => {
   it("should be able to read logs from latest block when no fromBlock is configured", async () => {
     const currentHeight = 10n;
     const blocksAhead = 1n;
+    const blockBatchSize = 100n;
     givenEvmBlockRepository(currentHeight, blocksAhead);
     givenMetadataRepository();
     givenStatsRepository();
@@ -53,7 +54,7 @@ describe("PollEvm", () => {
           addresses: cfg.filters[0].addresses,
           topics: cfg.filters[0].topics,
           fromBlock: currentHeight + blocksAhead,
-          toBlock: currentHeight + blocksAhead,
+          toBlock: currentHeight + blocksAhead + blockBatchSize,
         })
     );
   });
