57f7d8da4d1a079d984d29cad7462a1959142495
julian merlo <julianmerlo@julians-MacBook-Pro-2.local>
2024-06-27 15:31:27 -0300

    Improve comments

--- blockchain-watcher/src/domain/actions/evm/PollEvm.ts
+++ blockchain-watcher/src/domain/actions/evm/PollEvm.ts
@@ -111,17 +111,16 @@ export class PollEvm extends RunPollingJob {
     }
 
     let toBlock = BigInt(fromBlock) + BigInt(this.cfg.getBlockBatchSize());
-    // limit toBlock to obtained block height
+    // Limit toBlock to obtained block height
     if (toBlock > fromBlock && toBlock > latestBlockHeight) {
-      // We validate that the difference between the latest block and the toBlock is less/equal than 1100
-      // because the latestBlockHeight could return a block that is not the latest one and we want to avoid
-      // this block because we will re process old blocks
+      // We validate that the difference between the latest block and the toBlock is less/equal than 1100 (is our max batch size depending on the chain)
+      // because the latestBlockHeight could return a block that is not the latest one and we want to avoid this block because we will re process old blocks
       const diff = toBlock - latestBlockHeight;
       if (diff <= 1100) {
         toBlock = latestBlockHeight;
       }
     }
-    // limit toBlock to configured toBlock
+    // Limit toBlock to configured toBlock
     if (this.cfg.toBlock && toBlock > this.cfg.toBlock) {
       toBlock = this.cfg.toBlock;
     }
