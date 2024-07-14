615db6a021563ea962c20c8b30ccb7b92b77c063
julian merlo <julianmerlo@A52-de-Katherine.localdomain>
2024-07-14 18:04:12 -0500

    Validate if we have an error and finish the process

--- blockchain-watcher/src/infrastructure/repositories/cosmos/CosmosJsonRPCBlockRepository.ts
+++ blockchain-watcher/src/infrastructure/repositories/cosmos/CosmosJsonRPCBlockRepository.ts
@@ -68,6 +68,7 @@ export class CosmosJsonRPCBlockRepository implements CosmosRepository {
             "getTransactions",
             chain
           );
+          continuesFetching = false;
         }
       }
 
