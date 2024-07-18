c93b229f351d1c371f235f22a35a1fb139b2591e
Mariano <9205080+marianososto@users.noreply.github.com>
2024-07-18 17:00:05 -0300

    add to fly/migration.sql the creation of wh_operation-_transactions

--- fly/migration/migration.sql
+++ fly/migration/migration.sql
@@ -1,58 +1,95 @@
 -- This file contains the SQL queries to create the tables for the wormhole schema
 -- create table wormhole.wh_observations
-CREATE TABLE wormhole.wh_observations (
-    "id" varchar not null,
-    "emitter_chain_id" smallint not null,
-    "emitter_address" varchar not null,
-    "sequence" decimal(20,0) not null,
-    "hash" varchar not null,
-    "tx_hash" varchar not null,
-    "guardian_address" varchar not null,
-    "signature" bytea not null,
-    "created_at" timestamptz not null,
-    "updated_at" timestamptz not null,
+CREATE TABLE wormhole.wh_observations
+(
+    "id"               varchar        not null,
+    "emitter_chain_id" smallint       not null,
+    "emitter_address"  varchar        not null,
+    "sequence"         decimal(20, 0) not null,
+    "hash"             varchar        not null,
+    "tx_hash"          varchar        not null,
+    "guardian_address" varchar        not null,
+    "signature"        bytea          not null,
+    "created_at"       timestamptz    not null,
+    "updated_at"       timestamptz    not null,
     PRIMARY KEY (id)
 );
 CREATE INDEX "wh_observations_hash_idx"
     ON wh_observations ("hash");
 CREATE INDEX "wh_observations_tx_hash_idx"
     ON wh_observations ("tx_hash");
-CREATE INDEX "wh_observations_emitter_chain_id_emitter_address_sequence_idx" 
+CREATE INDEX "wh_observations_emitter_chain_id_emitter_address_sequence_idx"
     on wh_observations ("emitter_chain_id", "emitter_address", "sequence");
 
 -- create table wormhole.wh_attestation_vaas
-CREATE TABLE wormhole.wh_attestation_vaas (
-    "id" varchar not null,
-    "vaa_id" varchar not null,
-    "version" smallint not null,
-    "emitter_chain_id" smallint not null,
-    "emitter_address" varchar not null,
-    "sequence" decimal(20,0) not null,
-    "guardian_set_index" bigint not null,
-    "raw" bytea not null,
-    "timestamp" timestamptz not null,
-    "active" boolean not null,
-    "is_duplicated" boolean not null,
-    "created_at" timestamptz not null,
-    "updated_at" timestamptz not null,
-   PRIMARY KEY (id)
+CREATE TABLE wormhole.wh_attestation_vaas
+(
+    "id"                 varchar        not null,
+    "vaa_id"             varchar        not null,
+    "version"            smallint       not null,
+    "emitter_chain_id"   smallint       not null,
+    "emitter_address"    varchar        not null,
+    "sequence"           decimal(20, 0) not null,
+    "guardian_set_index" bigint         not null,
+    "raw"                bytea          not null,
+    "timestamp"          timestamptz    not null,
+    "active"             boolean        not null,
+    "is_duplicated"      boolean        not null,
+    "created_at"         timestamptz    not null,
+    "updated_at"         timestamptz    not null,
+    PRIMARY KEY (id)
 );
 
-CREATE INDEX "wh_attestation_vaas_vaa_id_idx" 
+CREATE INDEX "wh_attestation_vaas_vaa_id_idx"
     ON wormhole.wh_attestation_vaas ("vaa_id");
-CREATE INDEX "wh_attestation_vaas_emitter_chain_id_idx" 
+CREATE INDEX "wh_attestation_vaas_emitter_chain_id_idx"
     ON wormhole.wh_attestation_vaas ("emitter_chain_id");
-CREATE INDEX "wh_attestation_vaas_emitter_chain_id_emitter_address_idx" 
-    ON wormhole.wh_attestation_vaas ("emitter_chain_id","emitter_address");
-CREATE INDEX "wh_attestation_vaas_timestamp_idx" 
+CREATE INDEX "wh_attestation_vaas_emitter_chain_id_emitter_address_idx"
+    ON wormhole.wh_attestation_vaas ("emitter_chain_id", "emitter_address");
+CREATE INDEX "wh_attestation_vaas_timestamp_idx"
     ON wormhole.wh_attestation_vaas ("timestamp" desc);
 
 -- create table wormhole.wh_governor_status
-CREATE TABLE wormhole.wh_governor_status (
-	id varchar NOT NULL,
-	guardian_name varchar NOT NULL,
-	message jsonb NOT NULL,
-	created_at timestamptz NOT NULL,
-	updated_at timestamptz NOT NULL,
-	CONSTRAINT wh_governor_status_pkey PRIMARY KEY (id)
+CREATE TABLE wormhole.wh_governor_status
+(
+    id            varchar     NOT NULL,
+    guardian_name varchar     NOT NULL,
+    message       jsonb       NOT NULL,
+    created_at    timestamptz NOT NULL,
+    updated_at    timestamptz NOT NULL,
+    CONSTRAINT wh_governor_status_pkey PRIMARY KEY (id)
 );
+
+CREATE TABLE wh_operation_transactions
+(
+    "chain_id"            smallint       not null,
+    "tx_hash"             varchar        not null,
+    "type"                varchar        not null,
+    "created_at"          timestamp      not null,
+    "updated_at"          timestamp      not null,
+    "attestation_vaas_id" varchar        not null,
+    "vaa_id"              varchar        not null,
+    "status"              varchar        null,
+    "from_address"        varchar        null,
+    "to_address"          varchar        null,
+    "block_number"        decimal(20, 0) null,
+    "blockchain_method"   varchar        null,
+    "fee"                 varchar        null,
+    "raw_fee"             json           null,
+    "timestamp"           timestamptz    not null,
+    "rpc_response"        json           null,
+    PRIMARY KEY wh_operation_transactions_pk(chain_id, tx_hash)
+);
+CREATE INDEX "wh_operation_transactions_vaa_id_idx"
+    ON wh_operation_transactions ("vaa_id");
+CREATE INDEX "wh_operation_transactions_tx_hash_idx"
+    ON wh_operation_transactions ("tx_hash");
+CREATE INDEX "wh_operation_transactions_from_address_idx"
+    ON wh_operation_transactions ("from_address");
+CREATE INDEX "wh_operation_transactions_to_address_idx"
+    ON wh_operation_transactions ("to_address");
+CREATE INDEX "wh_operation_transactions_chain_id_type_idx"
+    ON wh_operation_transactions ("chain_id", "type");
+CREATE INDEX "wh_attestation_vaas_timestamp_idx"
+    ON wh_attestation_vaas ("timestamp" desc);
+
