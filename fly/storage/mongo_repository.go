1a31925eb444bee5cc944fa6cd72d423837c7698
Mariano <9205080+marianososto@users.noreply.github.com>
2024-07-30 22:10:18 -0300

    fix broken merge

--- fly/storage/mongo_repository.go
+++ fly/storage/mongo_repository.go
@@ -9,14 +9,15 @@ import (
 	"time"
 
 	gossipv1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
-	"github.com/wormhole-foundation/wormhole-explorer/common/db"
+	eth_common "github.com/ethereum/go-ethereum/common"
+	"github.com/wormhole-foundation/wormhole-explorer/common/client/alert"
 	"github.com/wormhole-foundation/wormhole-explorer/common/domain"
+	"github.com/wormhole-foundation/wormhole-explorer/common/events"
+	"github.com/wormhole-foundation/wormhole-explorer/common/repository"
 	"github.com/wormhole-foundation/wormhole-explorer/common/utils"
 	"github.com/wormhole-foundation/wormhole-explorer/fly/event"
+	flyAlert "github.com/wormhole-foundation/wormhole-explorer/fly/internal/alert"
 	"github.com/wormhole-foundation/wormhole-explorer/fly/internal/metrics"
-<<<<<<<< HEAD:fly/storage/repository.go
-	sdk "github.com/wormhole-foundation/wormhole/sdk/vaa"
-========
 	"github.com/wormhole-foundation/wormhole-explorer/fly/internal/track"
 	"github.com/wormhole-foundation/wormhole-explorer/fly/producer"
 	"github.com/wormhole-foundation/wormhole-explorer/fly/txhash"
@@ -25,18 +26,10 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
->>>>>>>> 0607a9d8301b7d48faa9caa5a24a9a2359b094ff:fly/storage/mongo_repository.go
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/proto"
 )
 
-<<<<<<<< HEAD:fly/storage/repository.go
-// Repository is a storage repository.
-type Repository struct {
-	db      *db.DB
-	metrics metrics.Metrics
-	// TODO: after migration move eventDispatcher to handlers.
-========
 // TODO remove repository after switch to postgres db.
 // TODO separate and maybe share between fly and web
 type MongoRepository struct {
@@ -45,27 +38,20 @@ type MongoRepository struct {
 	db              *mongo.Database
 	afterUpdate     producer.PushFunc
 	txHashStore     txhash.TxHashStore
->>>>>>>> 0607a9d8301b7d48faa9caa5a24a9a2359b094ff:fly/storage/mongo_repository.go
 	eventDispatcher event.EventDispatcher
-	logger          *zap.Logger
-}
-
-// // NewRepository creates a new storage repository.
-func NewRepository(db *db.DB, metrics metrics.Metrics,
-	eventDispatcher event.EventDispatcher, logger *zap.Logger) *Repository {
-	return &Repository{
-		db:              db,
-		metrics:         metrics,
-		eventDispatcher: eventDispatcher,
-		logger:          logger,
+	log             *zap.Logger
+	collections     struct {
+		vaas           *mongo.Collection
+		heartbeats     *mongo.Collection
+		observations   *mongo.Collection
+		governorConfig *mongo.Collection
+		governorStatus *mongo.Collection
+		vaasPythnet    *mongo.Collection
+		vaaCounts      *mongo.Collection
+		duplicateVaas  *mongo.Collection
 	}
 }
 
-<<<<<<<< HEAD:fly/storage/repository.go
-// UpsertObservation upserts an observation.
-func (r *Repository) UpsertObservation(ctx context.Context, o *gossipv1.SignedObservation, saveTxHash bool) error {
-	// id = {message_id}/{guardian_address}/{hash}
-========
 // TODO wrap repository with a service that filters using redis
 func NewMongoRepository(alertService alert.AlertClient, metrics metrics.Metrics,
 	db *mongo.Database,
@@ -176,101 +162,25 @@ func (s *MongoRepository) UpsertVAA(ctx context.Context, v *sdk.VAA, serializedV
 func (s *MongoRepository) UpsertObservation(ctx context.Context, o *gossipv1.SignedObservation, saveTxHash bool) error {
 	vaaID := strings.Split(o.MessageId, "/")
 	chainIDStr, emitter, sequenceStr := vaaID[0], vaaID[1], vaaID[2]
->>>>>>>> 0607a9d8301b7d48faa9caa5a24a9a2359b094ff:fly/storage/mongo_repository.go
 	id := fmt.Sprintf("%s/%s/%s", o.MessageId, hex.EncodeToString(o.Addr), hex.EncodeToString(o.Hash))
-
-	// current time
 	now := time.Now()
 
-	// get emitterChainID, emitterAddress, sequence
-	messageID := strings.Split(o.MessageId, "/")
-	strEmitterChainID, emitterAddress, strSequence := messageID[0], messageID[1], messageID[2]
-	chainIDUint64, err := strconv.ParseUint(strEmitterChainID, 10, 16)
+	chainIDUint, err := strconv.ParseUint(chainIDStr, 10, 16)
 	if err != nil {
-		r.logger.Error("Error parsing chainId",
-			zap.String("messageId", o.MessageId),
-			zap.Error(err))
-
-		return err
-	}
-	emitterChainID := sdk.ChainID(chainIDUint64)
-	sequence, err := strconv.ParseUint(strSequence, 10, 64)
-	if err != nil {
-		r.logger.Error("Error parsing sequence",
-			zap.String("messageId", o.MessageId),
-			zap.Error(err))
+		s.log.Error("Error parsing chainId", zap.Error(err))
 		return err
 	}
 
-<<<<<<<< HEAD:fly/storage/repository.go
-	// guardian address
-	guardianAddress := utils.NormalizeHex(hex.EncodeToString(o.Addr))
-
-	// hash
-	hash := hex.EncodeToString(o.Hash)
-
-	// native txHash
-	txHash, err := domain.EncodeTrxHashByChainID(emitterChainID, o.GetTxHash())
-========
 	// TODO should we notify the caller that pyth observations are not stored?
 	if sdk.ChainID(chainIDUint) == sdk.ChainIDPythNet {
 		return nil
 	}
 	sequence, err := strconv.ParseUint(sequenceStr, 10, 64)
->>>>>>>> 0607a9d8301b7d48faa9caa5a24a9a2359b094ff:fly/storage/mongo_repository.go
 	if err != nil {
-		r.logger.Warn("Error encoding tx hash",
-			zap.String("messageId", o.MessageId),
-			zap.ByteString("txHash", o.GetTxHash()),
-			zap.Error(err))
-
-		r.metrics.IncObservationWithoutTxHash(emitterChainID)
-	}
-
-	query := `
-		INSERT INTO wormhole.wh_observations 
-		(id, emitter_chain_id, emitter_address, "sequence", hash, tx_hash, guardian_address, signature, created_at)  
-		VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) 
-		ON CONFLICT(id) DO UPDATE 
-		SET tx_hash = $6, signature = $8, updated_at = $10 
-		RETURNING updated_at;
-		`
-
-	var result *time.Time
-	err = r.db.ExecAndScan(ctx,
-		&result,
-		query,
-		id,
-		emitterChainID,
-		emitterAddress,
-		sequence,
-		hash,
-		txHash,
-		guardianAddress,
-		o.Signature,
-		now,
-		now)
-
-	if err != nil {
-		r.logger.Error("Error upserting observation",
-			zap.String("messageId", o.MessageId),
-			zap.Error(err))
+		s.log.Error("Error parsing sequence", zap.Error(err))
 		return err
 	}
 
-<<<<<<<< HEAD:fly/storage/repository.go
-	rowInserted := isRowInserted(result)
-	if rowInserted {
-		r.metrics.IncObservationInserted(emitterChainID)
-	}
-
-	return nil
-}
-
-// UpsertVAA upserts a VAA.
-func (r *Repository) UpsertVAA(ctx context.Context, v *sdk.VAA, serializedVaa []byte) error {
-	id := utils.NormalizeHex(v.HexDigest()) //digest
-========
 	chainID := sdk.ChainID(chainIDUint)
 	var nativeTxHash string
 	switch chainID {
@@ -360,72 +270,37 @@ func (s *MongoRepository) ReplaceVaaTxHash(ctx context.Context, vaaID, oldTxHash
 
 func (s *MongoRepository) UpsertHeartbeat(hb *gossipv1.Heartbeat) error {
 	id := hb.GuardianAddr
->>>>>>>> 0607a9d8301b7d48faa9caa5a24a9a2359b094ff:fly/storage/mongo_repository.go
 	now := time.Now()
-
-<<<<<<<< HEAD:fly/storage/repository.go
-	table := "wormhole.wh_attestation_vaas"
-	if v.EmitterChain == sdk.ChainIDPythNet {
-		table = "wormhole.wh_attestation_vaas_pythnet"
+	update := bson.D{{Key: "$set", Value: hb}, {Key: "$set", Value: bson.D{{Key: "updatedAt", Value: now}}}, {Key: "$setOnInsert", Value: bson.D{{Key: "indexedAt", Value: now}}}}
+	opts := options.Update().SetUpsert(true)
+	_, err := s.collections.heartbeats.UpdateByID(context.TODO(), id, update, opts)
+	if err != nil {
+		s.log.Error("Error inserting heartbeat", zap.Error(err))
+		// send alert when exists an error saving heartbeat.
+		alertContext := alert.AlertContext{
+			Details: map[string]string{
+				"guardianAddr": hb.GuardianAddr,
+				"nodeName":     hb.NodeName,
+			},
+			Error: err,
+		}
+		s.alertClient.CreateAndSend(context.TODO(), flyAlert.ErrorSaveHeartbeat, alertContext)
 	}
+	return err
+}
 
-	queryTemplate := `
-	INSERT INTO %s 
-	(id, vaa_id, "version", emitter_chain_id, emitter_address, "sequence", guardian_set_index,
-	raw, "timestamp", active, is_duplicated, created_at) 
-	VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) 
-	ON CONFLICT(id) DO UPDATE 
-	SET vaa_id = $2, version =$3, emitter_chain_id = $4, emitter_address = $5, "sequence" = $6, guardian_set_index = $7, 
-	raw = $8, "timestamp" = $9, updated_at = $13 
-	RETURNING updated_at;
-	`
-
-	//RETURNING id, updated_at;
-	query := fmt.Sprintf(queryTemplate, table)
-
-	var result *time.Time
-	err := r.db.ExecAndScan(ctx,
-		&result,
-		query,
-		id,
-		v.MessageID(),
-		v.Version,
-		v.EmitterChain,
-		v.EmitterAddress,
-		v.Sequence,
-		v.GuardianSetIndex,
-		serializedVaa,
-		v.Timestamp,
-		true,
-		false,
-		now,
-		now)
-
-========
 func (s *MongoRepository) UpsertGovernorConfig(ctx context.Context, govC *gossipv1.SignedChainGovernorConfig) error {
 	id := hex.EncodeToString(govC.GuardianAddr)
 	now := time.Now()
 	var gCfg gossipv1.ChainGovernorConfig
 	err := proto.Unmarshal(govC.Config, &gCfg)
->>>>>>>> 0607a9d8301b7d48faa9caa5a24a9a2359b094ff:fly/storage/mongo_repository.go
 	if err != nil {
-		r.logger.Error("Error upserting VAA",
-			zap.String("id", id),
-			zap.String("vaaId", v.MessageID()),
-			zap.Error(err))
+		s.log.Error("Error unmarshalling govr config", zap.Error(err))
 		return err
 	}
 
-	rowInserted := isRowInserted(result)
-	if rowInserted {
-		r.metrics.IncVaaInserted(v.EmitterChain)
+	cfg := toGovernorConfigUpdate(&gCfg)
 
-<<<<<<<< HEAD:fly/storage/repository.go
-		vaa := event.Vaa{
-			ID:               id,
-			VaaID:            v.MessageID(),
-			EmitterChainID:   uint16(v.EmitterChain),
-========
 	update := bson.D{{Key: "$set", Value: govC}, {Key: "$set", Value: bson.D{{Key: "parsedConfig", Value: cfg}}}, {Key: "$set", Value: bson.D{{Key: "updatedAt", Value: now}}}, {Key: "$setOnInsert", Value: bson.D{{Key: "createdAt", Value: now}}}}
 
 	opts := options.Update().SetUpsert(true)
@@ -704,65 +579,20 @@ func (s *MongoRepository) notifyNewVaa(ctx context.Context, v *sdk.VAA, serializ
 		events.SignedVaa{
 			ID:               v.MessageID(),
 			EmitterChain:     uint16(v.EmitterChain),
->>>>>>>> 0607a9d8301b7d48faa9caa5a24a9a2359b094ff:fly/storage/mongo_repository.go
 			EmitterAddress:   v.EmitterAddress.String(),
 			Sequence:         v.Sequence,
-			Version:          v.Version,
 			GuardianSetIndex: v.GuardianSetIndex,
-			Raw:              serializedVaa,
 			Timestamp:        v.Timestamp,
-		}
-		// dispatch new VAA event to the pipeline.
-		// TODO:
-		// -> define in spy component how to handle txHash because we dont have the txHash.
-		// -> check mongo repo events.NewNotificationEvent[events.SignedVaa]
-		err := r.eventDispatcher.NewVaa(ctx, vaa)
-		if err != nil {
-			r.logger.Error("Error dispatching new VAA event",
-				zap.String("id", id),
-				zap.String("vaaId", v.MessageID()),
-				zap.Error(err))
-			return err
-		}
+			Vaa:              serializedVaa,
+			TxHash:           txHash,
+			Version:          int(v.Version),
+		})
+	if newErr != nil {
+		return newErr
 	}
-	return nil
+	return s.afterUpdate(ctx, &producer.Notification{ID: v.MessageID(), Event: event, EmitterChain: v.EmitterChain})
 }
 
-<<<<<<<< HEAD:fly/storage/repository.go
-// UpsertHeartbeat upserts a heartbeat.
-// Questions: sWe need to support this in the v2??
-func (r *Repository) UpsertHeartbeat(hb *gossipv1.Heartbeat) error {
-	id := utils.NormalizeHex(hb.GuardianAddr)
-	now := time.Now()
-	timestamp := time.Unix(0, hb.Timestamp)
-	bootTimestamp := time.Unix(0, hb.BootTimestamp)
-
-	query := `
-	INSERT INTO wormhole.wh_heartbeats
-	(id, guardian_name, boot_timestamp, "timestamp", version, networks, feature, created_at, updated_at)
-	VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) 
-	ON CONFLICT(id) DO UPDATE 
-	SET guardian_name = $2, boot_timestamp = $3, "timestamp" = $4, version = $5, networks = $6, feature = $7, updated_at = $9;
-	`
-
-	_, err := r.db.Exec(context.Background(),
-		query,
-		id,
-		hb.GetNodeName(),
-		bootTimestamp,
-		timestamp,
-		hb.Version,
-		hb.Networks,
-		hb.Features,
-		now,
-		now)
-
-	if err != nil {
-		r.logger.Error("Error upserting heartbeat",
-			zap.String("id", id),
-			zap.Error(err))
-		return err
-========
 func createDuplicateVaaUpdateFromVaa(uniqueID string, v *sdk.VAA, serializedVaa []byte, t time.Time) *DuplicateVaaUpdate {
 	return &DuplicateVaaUpdate{
 		ID:               uniqueID,
@@ -777,165 +607,5 @@ func createDuplicateVaaUpdateFromVaa(uniqueID string, v *sdk.VAA, serializedVaa 
 		UpdatedAt:        &t,
 		ConsistencyLevel: v.ConsistencyLevel,
 		Digest:           utils.NormalizeHex(v.HexDigest()),
->>>>>>>> 0607a9d8301b7d48faa9caa5a24a9a2359b094ff:fly/storage/mongo_repository.go
 	}
-
-	return nil
-}
-
-// UpsertGovernorConfig upserts a governor config.
-func (r *Repository) UpsertGovernorConfig(ctx context.Context, govC *gossipv1.SignedChainGovernorConfig) error {
-	// id is the guardian address.
-	id := hex.EncodeToString(govC.GuardianAddr)
-	now := time.Now()
-
-	// unmarshal governor config
-	var gc gossipv1.ChainGovernorConfig
-	err := proto.Unmarshal(govC.Config, &gc)
-	if err != nil {
-		r.logger.Error("Error unmarshalling governor config",
-			zap.String("id", id),
-			zap.Error(err))
-		return err
-	}
-
-	governorConfig := toGovernorConfigUpdate(&gc)
-	timestamp := time.Unix(0, governorConfig.Timestamp)
-
-	query := `
-	INSERT INTO wormhole.wh_governor_config
-	(id, guardian_name, counter, timestamp, tokens, created_at, updated_at)
-	VALUES($1, $2, $3, $4, $5, $6, $7) 
-	ON CONFLICT(id) DO UPDATE 
-	SET counter = $3, timestamp = $4, tokens = $5, updated_at = $7;
-	`
-
-	_, err = r.db.Exec(context.Background(),
-		query,
-		id,
-		gc.GetNodeName(),
-		governorConfig.Counter,
-		timestamp,
-		governorConfig.Tokens,
-		now,
-		now)
-
-	if err != nil {
-		r.logger.Error("Error upserting governor config",
-			zap.String("id", id),
-			zap.Error(err))
-		return err
-	}
-
-	// send governor config to topic. [fly-event-processor]
-	errDispatcher := r.eventDispatcher.NewGovernorConfig(context.TODO(), event.GovernorConfig{
-		NodeAddress: id,
-		NodeName:    governorConfig.NodeName,
-		Counter:     governorConfig.Counter,
-		Timestamp:   governorConfig.Timestamp,
-		Chains:      governorConfig.Chains,
-	})
-
-	if errDispatcher != nil {
-		r.logger.Error("Error sending governor config to topic",
-			zap.String("id", id),
-			zap.Error(errDispatcher))
-	}
-	return errDispatcher
-}
-
-// UpsertGovernorStatus upserts a governor status.
-func (r *Repository) UpsertGovernorStatus(ctx context.Context, govS *gossipv1.SignedChainGovernorStatus) error {
-	// id is the guardian address.
-	id := hex.EncodeToString(govS.GuardianAddr)
-	now := time.Now()
-
-	// unmarshal governor status
-	var gs gossipv1.ChainGovernorStatus
-	err := proto.Unmarshal(govS.Status, &gs)
-	if err != nil {
-		r.logger.Error("Error unmarshalling governor status",
-			zap.String("id", id),
-			zap.Error(err))
-		return err
-	}
-	governorStatus := toGovernorStatusUpdate(&gs)
-	timestamp := time.Unix(0, governorStatus.Timestamp)
-
-	query := `
-	INSERT INTO wormhole.wh_governor_status
-	(id, guardian_name, message, timestamp, created_at, updated_at)
-	VALUES($1, $2, $3, $4, $5, $6) 
-	ON CONFLICT(id) DO UPDATE 
-	SET message = $3, timestamp = $4, updated_at = $6;
-	`
-
-	_, err = r.db.Exec(context.Background(),
-		query,
-		id,
-		gs.GetNodeName(),
-		governorStatus,
-		timestamp,
-		now,
-		now)
-
-	if err != nil {
-		r.logger.Error("Error upserting governor status",
-			zap.String("id", id),
-			zap.Error(err))
-		return err
-	}
-
-	// send governor status to topic. [fly-event-processor]
-	errDispatcher := r.eventDispatcher.NewGovernorStatus(context.TODO(), event.GovernorStatus{
-		NodeAddress: id,
-		NodeName:    governorStatus.NodeName,
-		Counter:     governorStatus.Counter,
-		Timestamp:   governorStatus.Timestamp,
-		Chains:      governorStatus.Chains,
-	})
-
-	if errDispatcher != nil {
-		r.logger.Error("Error sending governor status to topic",
-			zap.String("id", id),
-			zap.Error(errDispatcher))
-	}
-	return errDispatcher
-}
-
-// ReplaceVaaTxHash replaces a VAA transaction hash.
-// Requiered method to support Storager interface
-// TODO: delete this methods after migration
-func (r *Repository) ReplaceVaaTxHash(ctx context.Context, vaaID string, oldTxHash string, newTxHash string) error {
-	return nil
-}
-
-// FindVaaByID finds a VAA by ID.
-// Requiered method to support Storager interface
-// TODO: delete this methods after migration
-func (r *Repository) FindVaaByID(ctx context.Context, vaaID string) (*VaaUpdate, error) {
-	return nil, nil
-}
-
-// FindVaaByChainID finds a VAA by chain ID.
-// Requiered method to support Storager interface
-// TODO: delete this methods after migration
-func (r *Repository) FindVaaByChainID(ctx context.Context, chainID sdk.ChainID, page int64, pageSize int64) ([]*VaaUpdate, error) {
-	return nil, nil
-}
-
-// UpsertDuplicateVaa upserts a duplicate VAA.
-// Requiered method to support Storager interface
-// TODO: delete this methods after migration
-func (r *Repository) UpsertDuplicateVaa(ctx context.Context, v *sdk.VAA, serializedVaa []byte) error {
-	return nil
-}
-
-// isRowInserted checks if a row was inserted.
-func isRowInserted(result *time.Time) bool {
-	isRowInserted := false
-	if result == nil {
-		isRowInserted = true
-	}
-	return isRowInserted
 }
