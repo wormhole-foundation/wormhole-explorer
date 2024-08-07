4fce02fab24cd475c10070e6d9b64edecef19c7b
ftocal <46001274+ftocal@users.noreply.github.com>
2024-08-07 16:51:55 -0300

    Add deploy to support fly in postgres mode as repository (#1593)
    Co-authored-by: walker-16 <agpazos85@gmail.com>

--- fly/config/config.go
+++ fly/config/config.go
@@ -28,8 +28,8 @@ const (
 )
 
 const (
-	RunModeMongo    = "mongo"
-	RunModePostgres = "postgres"
+	DbLayerMongo    = "mongo"
+	DbLayerPostgres = "postgres"
 )
 
 // P2pNetworkConfig config struct.
@@ -70,7 +70,7 @@ type Configuration struct {
 	EthereumUrl               string `env:"ETHEREUM_URL,required"`
 	DatabaseUrl               string `env:"DB_URL"`
 	DatabaseLogEnabled        bool   `env:"DB_LOG_ENABLED"`
-	RunMode                   string `env:"RUN_MODE,default=mongo"` // mongo, postgres. Default is mongo.
+	DbLayer                   string `env:"DB_LAYER,default=mongo"` // mongo, postgres. Default is mongo.
 }
 
 type RedisConfiguration struct {
