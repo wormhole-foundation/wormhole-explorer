71cfb4335da6ab229f22c1e1949104a8e1fccf43
Mariano <9205080+marianososto@users.noreply.github.com>
2024-08-09 16:17:05 -0300

    [1548] Migration of pipeline component to use postresql (#1570)
    * Add new database abstraction in common
    
    * fly - migrate observations to postgres
    
    * fly - migrate vaas and governor status to postgres
    
    * fly - add event dispatcher for vaa-pipeline
    
    * Add to repository on conflict behaviour to queries
    
    * Add observations and vaas metrics to repository
    
    * add support to vaa pythnet table
    
    * Add new postgres repository for guardian set in common
    
    * add common method to exec and scan
    
    * add query log
    
    * fly - add event dispatacher for governor config message
    
    * update migration.sql file
    
    * fix inserted flag issue, add metrics and event dispatcher for vaas and governor config
    
    * Add comment to pgx cmdTag bug and workaround for this cases
    
    * update pipeline vaa event & migration
    
    * Add in fly-event-processor support to governor config chain
    
    * Add in fly-event-processor governor_status postgres repository
    
    * Fix table wh_governor_config_chains
    
    * fix governor config chain flow
    
    * fly-event-processor - fix governor config flow
    
    * Add health for postgres in common
    
    * add dummy governor status processor and refactor run and build
    
    * Add comment in fly-event-processor
    
    * migration of pipeline component
    
    * finish full test
    
    * fix broken merge
    
    * add missing implementation
    
    * small changes
    
    * change condition
    
    * add unit-test to pipeline
    
    * add missing Close()
    
    * fix unit-test
    
    ---------
    
    Co-authored-by: Agustin Pazos <agpazos85@gmail.com>

--- pipeline/internal/metrics/metrics.go
+++ pipeline/internal/metrics/metrics.go
@@ -1,5 +1,7 @@
 package metrics
 
+import sdk "github.com/wormhole-foundation/wormhole/sdk/vaa"
+
 const serviceName = "wormscan-pipeline"
 
 // Metrics is a metrics interface.
@@ -9,4 +11,8 @@ type Metrics interface {
 
 	IncVaaWithoutTxHash(chainID uint16)
 	IncVaaWithTxHashFixed(chainID uint16)
+
+	IncVaaSendNotificationFromGossipSQS(chainID sdk.ChainID)
+	IncVaaFromGossipSQS(chainID uint16)
+	IncVaaFailedProcessing(chainID sdk.ChainID, reason string)
 }
---
+++ pipeline/internal/queue/event_sqs.go
@@ -0,0 +1,169 @@
+package queue
+
+import (
+	"context"
+	"encoding/json"
+	"strconv"
+	"sync"
+	"time"
+
+	"go.uber.org/zap"
+
+	sqsclient "github.com/wormhole-foundation/wormhole-explorer/common/client/sqs"
+	"github.com/wormhole-foundation/wormhole-explorer/pipeline/internal/metrics"
+)
+
+// SQSOption represents a VAA queue in SQS option function.
+type SQSOption func(*SQS)
+
+// SQS represents a VAA queue in SQS.
+type SQS struct {
+	consumer  *sqsclient.Consumer
+	ch        chan ConsumerMessage
+	converter ConverterFunc
+	chSize    int
+	wg        sync.WaitGroup
+	metrics   metrics.Metrics
+	logger    *zap.Logger
+}
+
+// FilterConsumeFunc filter vaaa func definition.
+type FilterConsumeFunc func(vaaEvent *VaaEvent) bool
+
+// ConverterFunc converts a message from a sqs message.
+type ConverterFunc func(string) (*Event, error)
+
+// NewEventSqs creates a VAA queue in SQS instances.
+func NewEventSqs(consumer *sqsclient.Consumer, converter ConverterFunc, metrics metrics.Metrics, logger *zap.Logger, opts ...SQSOption) *SQS {
+	s := &SQS{
+		consumer:  consumer,
+		chSize:    10,
+		metrics:   metrics,
+		converter: converter,
+		logger:    logger.With(zap.String("queueUrl", consumer.GetQueueUrl())),
+	}
+	for _, opt := range opts {
+		opt(s)
+	}
+	s.ch = make(chan ConsumerMessage, s.chSize)
+	return s
+}
+
+// WithChannelSize allows to specify a channel size when setting a value.
+func WithChannelSize(size int) SQSOption {
+	return func(d *SQS) {
+		d.chSize = size
+	}
+}
+
+// Consume returns the channel with the received messages from SQS queue.
+func (q *SQS) Consume(ctx context.Context) <-chan ConsumerMessage {
+	go func() {
+		for {
+			messages, err := q.consumer.GetMessages(ctx)
+			if err != nil {
+				q.logger.Error("Error getting messages from SQS", zap.Error(err))
+				continue
+			}
+			q.logger.Debug("Received messages from SQS", zap.Int("count", len(messages)))
+			expiredAt := time.Now().Add(q.consumer.GetVisibilityTimeout())
+			for _, msg := range messages {
+				// unmarshal body to sqsEvent
+				var sqsEvent sqsEvent
+				err := json.Unmarshal([]byte(*msg.Body), &sqsEvent)
+				if err != nil {
+					q.logger.Error("Error decoding message from SQS", zap.Error(err))
+					if err = q.consumer.DeleteMessage(ctx, msg.ReceiptHandle); err != nil {
+						q.logger.Error("Error deleting message from SQS", zap.Error(err))
+					}
+					continue
+				}
+
+				// unmarshal message to event
+				event, err := q.converter(sqsEvent.Message)
+				if err != nil {
+					q.logger.Error("Error converting event message", zap.Error(err))
+					if err = q.consumer.DeleteMessage(ctx, msg.ReceiptHandle); err != nil {
+						q.logger.Error("Error deleting message from SQS", zap.Error(err))
+					}
+					continue
+				}
+				if event == nil {
+					q.logger.Warn("Can not handle message", zap.String("body", *msg.Body))
+					if err = q.consumer.DeleteMessage(ctx, msg.ReceiptHandle); err != nil {
+						q.logger.Error("Error deleting message from SQS", zap.Error(err))
+					}
+					continue
+				}
+
+				retry, _ := strconv.Atoi(msg.Attributes["ApproximateReceiveCount"])
+				q.wg.Add(1)
+				q.ch <- &sqsConsumerMessage{
+					id:            msg.ReceiptHandle,
+					data:          event,
+					wg:            &q.wg,
+					logger:        q.logger,
+					consumer:      q.consumer,
+					expiredAt:     expiredAt,
+					sentTimestamp: sqsclient.GetSentTimestamp(msg),
+					retry:         uint8(retry),
+					metrics:       q.metrics,
+					ctx:           ctx,
+				}
+			}
+			q.wg.Wait()
+		}
+
+	}()
+	return q.ch
+}
+
+// Close closes all consumer resources.
+func (q *SQS) Close() {
+	close(q.ch)
+}
+
+type sqsConsumerMessage struct {
+	data          *Event
+	consumer      *sqsclient.Consumer
+	wg            *sync.WaitGroup
+	id            *string
+	logger        *zap.Logger
+	expiredAt     time.Time
+	sentTimestamp *time.Time
+	retry         uint8
+	metrics       metrics.Metrics
+	ctx           context.Context
+}
+
+func (m *sqsConsumerMessage) Data() *Event {
+	return m.data
+}
+
+func (m *sqsConsumerMessage) Done() {
+	if err := m.consumer.DeleteMessage(m.ctx, m.id); err != nil {
+		m.logger.Error("Error deleting message from SQS",
+			zap.String("vaaId", m.data.ID),
+			zap.Bool("isExpired", m.IsExpired()),
+			zap.Time("expiredAt", m.expiredAt),
+			zap.Error(err),
+		)
+	}
+	m.wg.Done()
+}
+
+func (m *sqsConsumerMessage) Failed() {
+	m.wg.Done()
+}
+
+func (m *sqsConsumerMessage) IsExpired() bool {
+	return m.expiredAt.Before(time.Now())
+}
+
+func (m *sqsConsumerMessage) Retry() uint8 {
+	return m.retry
+}
+
+func (m *sqsConsumerMessage) SentTimestamp() *time.Time {
+	return m.sentTimestamp
+}
