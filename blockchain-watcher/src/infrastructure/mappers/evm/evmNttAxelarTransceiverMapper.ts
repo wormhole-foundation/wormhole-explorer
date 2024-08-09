2dfbf8f5daba85b87fd912a021cb0209b0e3ac07
Abhishek Rajput <abhi@xlabs.xyz>
2024-08-10 01:34:14 +0530

    Fix: Add source token as tag | other improvements (#1599)
    * Fix source token parsing|other fixes
    
    * Add blocknumber as bigint

--- blockchain-watcher/src/infrastructure/mappers/evm/evmNttAxelarTransceiverMapper.ts
+++ blockchain-watcher/src/infrastructure/mappers/evm/evmNttAxelarTransceiverMapper.ts
@@ -4,7 +4,8 @@ import { ethers } from "ethers";
 import {
   EVMNTTManagerAttributes,
   extractDigestFromNttPayload,
-  TransceiverLogData,
+  NTTTransfer,
+  parseNttPayload,
 } from "./helpers/ntt";
 import { toChainId, ChainId } from "@wormhole-foundation/sdk-base";
 import {
@@ -47,32 +48,34 @@ export const evmNttAxelarTransceiverMapper = (
       from: transaction.from,
       to: transaction.to,
       status: txnStatus,
-      blockNumber: transaction.blockNumber,
+      blockNumber: BigInt(transaction.blockNumber),
       timestamp: transaction.timestamp,
       txHash: transaction.hash,
-      gas: transaction.gas,
-      gasPrice: transaction.gasPrice,
-      gasUsed: transaction.gasUsed,
-      effectiveGasPrice: transaction.effectiveGasPrice,
+      gas: BigInt(transaction.gas),
+      gasPrice: BigInt(transaction.gasPrice),
+      gasUsed: BigInt(transaction.gasUsed),
+      effectiveGasPrice: BigInt(transaction.effectiveGasPrice),
       nonce: transaction.nonce,
       cost: BigInt(transaction.gasUsed) * BigInt(transaction.effectiveGasPrice),
       protocol: "NTT",
       // We use digest as an unique identifier for the NTT transfer events across source and target chains
       digest: transceiverInfo.digest,
+      amount: transceiverInfo?.amount,
     },
     tags: {
       recipientChain: transceiverInfo.recipientChain,
       emitterChain: emitterChainId,
       transceiverType: transceiverInfo.transceiverType,
+      sourceToken: transceiverInfo?.sourceToken,
     },
   };
 };
 
 // SendTransceiverMessage (index_topic_1 uint16 recipientChainId, bytes nttManagerMessage, index_topic_2 bytes32 recipientNttManagerAddress, index_topic_3 bytes32 refundAddress)
-export const mapLogDataFromAxelarSendTransceiverMessage: LogMapperFn<TransceiverLogData> = (
+export const mapLogDataFromAxelarSendTransceiverMessage: LogMapperFn<NTTTransfer | undefined> = (
   log: EvmTransactionLog,
   emitterChainId: ChainId
-): TransceiverLogData | undefined => {
+): NTTTransfer | undefined => {
   try {
     const iface = new ethers.utils.Interface(AXELAR_SEND_TRANSCEIVER_MESSAGE_ABI);
     const parsedLog = iface.parseLog(log);
@@ -80,19 +83,22 @@ export const mapLogDataFromAxelarSendTransceiverMessage: LogMapperFn<Transceiver
     let nttManagerPayload = parsedLog.args.nttManagerMessage;
 
     const calculatedDigest = extractDigestFromNttPayload(nttManagerPayload, emitterChainId);
+    const parsedNttPayload = parseNttPayload(nttManagerPayload);
 
     return {
       eventName: "ntt-send-transceiver-message",
       transceiverType: "axelar",
       recipientChain: toChainId(parsedLog.args.recipientChainId),
       digest: calculatedDigest,
+      sourceToken: Buffer.from(parsedNttPayload?.payload?.sourceToken).toString("hex"),
+      amount: parsedNttPayload?.payload?.trimmedAmount?.amount,
     };
   } catch (err) {
     logger.error(`Error parsing axelar send transceiver message: ${err}`);
   }
 };
 
-const TRANSCEIVER_TOPICS: Record<string, LogMapperFn<TransceiverLogData | undefined>> = {
+const TRANSCEIVER_TOPICS: Record<string, LogMapperFn<NTTTransfer | undefined>> = {
   "0xcdba4baae54ffe4453599128e176cfa8a3190fff44e9f60a444875db7fb0572a":
     mapLogDataFromAxelarSendTransceiverMessage,
 };
