ad9e22a82049475e1dfd20681d516359264fcee6
Julian <52217955+julianmerlo95@users.noreply.github.com>
2024-09-11 08:33:49 -0300

    [Blockchain Watcher] (SOLANA) Create wrapper and offline method (#1669)
    * Create wrapper and offline method
    
    * Improve comments
    
    * Improve InstrumentedConnectionWrapper
    
    ---------
    
    Co-authored-by: julian merlo <julianmerlo@julians-MacBook-Air.local>

---
+++ blockchain-watcher/src/infrastructure/rpc/http/InstrumentedConnectionWrapper.ts
@@ -0,0 +1,26 @@
+import { Commitment, Connection, ConnectionConfig } from "@solana/web3.js";
+import { ProviderHealthInstrumentation } from "@xlabs/rpc-pool";
+
+export class InstrumentedConnectionWrapper extends Connection {
+  health: ProviderHealthInstrumentation;
+  private url: string;
+
+  constructor(
+    endpoint: string,
+    commitment: Commitment | ConnectionConfig,
+    timeout: number,
+    chain: string
+  ) {
+    super(endpoint, commitment);
+    this.health = new ProviderHealthInstrumentation(timeout, chain);
+    this.url = endpoint;
+  }
+
+  public setProviderOffline(): void {
+    this.health.serviceOfflineSince = new Date();
+  }
+
+  public getUrl(): string {
+    return this.url;
+  }
+}
