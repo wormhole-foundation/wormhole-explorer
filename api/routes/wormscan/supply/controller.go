60ff8f88eb7d8318f33bae72fd03ab2e70085e5a
Agustin Pazos <agpazos85@gmail.com>
2024-10-07 16:07:25 -0300

    modify supply endpoint to response in json format, add docs

--- api/routes/wormscan/supply/controller.go
+++ api/routes/wormscan/supply/controller.go
@@ -19,14 +19,37 @@ func NewController(serv *supply.Service, logger *zap.Logger) *Controller {
 	return &Controller{srv: serv, logger: logger.With(zap.String("module", "CirculatingSupplyService"))}
 }
 
-func (c *Controller) GetCirculatingSupply(ctx *fiber.Ctx) error {
-	supply := c.srv.GetCurrentCirculatingSupply(ctx.Context())
-	ctx.SendString(strconv.Itoa(supply.Unlocked))
-	return nil
+type CirculatingSupplyResponse struct {
+	CirculatingSupply string `json:"circulating_supply"`
 }
 
+// GetCirculatingSupply godoc
+// @Description Get W token circulation supply.
+// @Tags wormholescan
+// @ID supply
+// @Success 200 {object} CirculatingSupplyResponse
+// @Router /api/v1/supply [get]
+func (c *Controller) GetCirculatingSupply(ctx *fiber.Ctx) error {
+	supply := c.srv.GetCurrentCirculatingSupply(ctx.Context())
+	return ctx.JSON(CirculatingSupplyResponse{
+		CirculatingSupply: strconv.Itoa(supply.Unlocked),
+	})
+}
+
+type TotalSupplyResponse struct {
+	TotalSupply string `json:"total_supply"`
+}
+
+// GetTotalSupply godoc
+// @Description Get W token total supply.
+// @Tags wormholescan
+// @ID total-supply
+// @Success 200 {object} TotalSupplyResponse
+// @Router /api/v1/total-supply [get]
 func (c *Controller) GetTotalSupply(ctx *fiber.Ctx) error {
 	supply := c.srv.GetTotalSupply(ctx.Context())
-	ctx.SendString(strconv.Itoa(supply))
-	return nil
+
+	return ctx.JSON(TotalSupplyResponse{
+		TotalSupply: strconv.Itoa(supply),
+	})
 }
