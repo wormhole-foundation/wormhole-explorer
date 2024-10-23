253e13d2bf9d02bfb939bd6868b9e8efeef70bfc
Julian <52217955+julianmerlo95@users.noreply.github.com>
2024-10-23 16:54:17 -0300

    [Blockchain Watcher](FIX - CIRCLE) Validate payload parser (#1827)
    * Validate parse
    
    * Map Sui domain
    
    ---------
    
    Co-authored-by: julian merlo <julianmerlo@julians-MacBook-Air.local>

--- blockchain-watcher/src/infrastructure/mappers/utils/circle.ts
+++ blockchain-watcher/src/infrastructure/mappers/utils/circle.ts
@@ -4,7 +4,9 @@ export const toCirceChain = (env: string, domain: number) => {
   // Remove this when the SDK is updated to accept Noble as a domain with 4 value
   // @wormhole-foundation/sdk-base/dist/cjs/constants/circle.d.ts
   const environment = env === "mainnet" ? "Mainnet" : "Testnet";
-  return domain === 4 ? "Noble" : circle.toCircleChain(environment, domain);
+  if (domain === 4) return "Noble";
+  if (domain === 8) return "Sui";
+  return circle.toCircleChain(environment, domain);
 };
 
 export enum MessageProtocol {
